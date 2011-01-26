bf824cbd58ec2c750e87874b33323c33a7f47987
Christophe Delaere <christophe.delaere@cern.ch>
2011-01-26 16:45:20 +0000

    added formating for some plots

--- test/testcombinePlots.py
+++ test/testcombinePlots.py
@@ -22,19 +22,19 @@ process.CombinePlots = cms.PSet(
    cms.PSet(
      fileName = cms.string('controlPlots_Zbb-TuneZ2.root'),
      color = cms.uint32(2),
-     scale = cms.double(0.01675), #NLO
+     scale = cms.double(0.017978), #NLO
      role = cms.string('Zbb')
    ), 
    cms.PSet(
      fileName = cms.string('controlPlots_Zcc-TuneZ2_2.root'),
      color = cms.uint32(4),
-     scale = cms.double(0.017625), #NLO
+     scale = cms.double(0.0189175), #NLO
      role = cms.string('Zcc')
    ), 
    cms.PSet(
      fileName = cms.string('controlPlots_DYJetsToLL_TuneD6T.root'),
      color = cms.uint32(3),
-     scale = cms.double(0.04878), #NNLO
+     scale = cms.double(0.0523572), #NNLO
      role = cms.string('Z+jets')
    )
   ),
@@ -46,6 +46,68 @@ process.CombinePlots = cms.PSet(
       logy = cms.untracked.bool(True),
       labelx = cms.untracked.string("Z -> #mu#mu invariant mass (GeV/c^{2})"),
       labely = cms.untracked.string("Events/GeV/c^{2}")
-    )
+    ),
+    cms.PSet(
+      name = cms.string('bestzmassEle'),
+      rebin = cms.untracked.uint32(10),
+      logx = cms.untracked.bool(False),
+      logy = cms.untracked.bool(True),
+      labelx = cms.untracked.string("Z -> ee invariant mass (GeV/c^{2})"),
+      labely = cms.untracked.string("Events/GeV/c^{2}")
+    ),
+    cms.PSet(
+      name = cms.string('bjet1pt'),
+      rebin = cms.untracked.uint32(10),
+      logx = cms.untracked.bool(False),
+      logy = cms.untracked.bool(True),
+      labelx = cms.untracked.string("Leading bjet Pt (GeV/c)"),
+      labely = cms.untracked.string("Events/10GeV/c")
+    ),
+    cms.PSet(
+      name = cms.string('bjet2pt'),
+      rebin = cms.untracked.uint32(10),
+      logx = cms.untracked.bool(False),
+      logy = cms.untracked.bool(True),
+      labelx = cms.untracked.string("Second bjet Pt (GeV/c)"),
+      labely = cms.untracked.string("Events/10GeV/c")
+    ),
+    cms.PSet(
+      name = cms.string('MET'),
+      logy = cms.untracked.bool(True),
+      labelx = cms.untracked.string("Missing Et (GeV)"),
+      labely = cms.untracked.string("Events/2GeV")
+    ),
+    cms.PSet(
+      name = cms.string('vecdptZbj1'),
+      rebin = cms.untracked.uint32(10),
+      logx = cms.untracked.bool(False),
+      logy = cms.untracked.bool(True),
+      labelx = cms.untracked.string("Pt imbalance between Z and leading bjet (GeV/c)"),
+      labely = cms.untracked.string("Events/10GeV/c")
+    ),
+    cms.PSet(
+      name = cms.string('ZbM'),
+      rebin = cms.untracked.uint32(10),
+      labelx = cms.untracked.string("Zb invariant mass (GeV/c^{2})"),
+      labely = cms.untracked.string("Events/10GeV/c^{2}")
+    ),
+    cms.PSet(
+      name = cms.string('ZbPt'),
+      rebin = cms.untracked.uint32(10),
+      labelx = cms.untracked.string("Zb Pt (GeV/c)"),
+      labely = cms.untracked.string("Events/10GeV/c")
+    ),
+    cms.PSet(
+      name = cms.string('ZbbM'),
+      rebin = cms.untracked.uint32(10),
+      labelx = cms.untracked.string("Zbb invariant mass (GeV/c^{2})"),
+      labely = cms.untracked.string("Events/10GeV/c^{2}")
+    ),
+    cms.PSet(
+      name = cms.string('ZbbPt'),
+      rebin = cms.untracked.uint32(10),
+      labelx = cms.untracked.string("Zbb Pt (GeV/c)"),
+      labely = cms.untracked.string("Events/10GeV/c")
+    ),
   )
 )
