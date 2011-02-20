f3c53b804d87321d26fa13c4a353b7ea56a8382b
Christophe Delaere <christophe.delaere@cern.ch>
2011-02-20 18:50:23 +0000

    various update: allows to run without data, nostack option, more rebinning options, and cleaning

--- bin/combinePlots.cc
+++ bin/combinePlots.cc
@@ -19,7 +19,9 @@
 class plotCombiner {
 
    public:
-     plotCombiner() {}
+     plotCombiner(edm::ParameterSet& options) {
+       _nostack = options.getUntrackedParameter("nostack",false);
+     }
      ~plotCombiner() {}
      void run(TDirectory* output) {
        CombineDir(_filesData, _filesMC, output);
@@ -51,30 +53,27 @@ class plotCombiner {
      std::vector<TDirectory*> _filesData;
      std::vector<TDirectory*> _filesMC;
      std::map<std::string, edm::ParameterSet> _styleTweaks;
+     bool _nostack;
 };
 
 void setTDRStyle() {
   TStyle *tdrStyle = new TStyle("tdrStyle","Style for P-TDR");
-
-// For the canvas:
+  // For the canvas:
   tdrStyle->SetCanvasBorderMode(0);
   tdrStyle->SetCanvasColor(kWhite);
   tdrStyle->SetCanvasDefH(600); //Height of canvas
   tdrStyle->SetCanvasDefW(600); //Width of canvas
   tdrStyle->SetCanvasDefX(0);   //POsition on screen
   tdrStyle->SetCanvasDefY(0);
-
-// For the Pad:
+  // For the Pad:
   tdrStyle->SetPadBorderMode(0);
-  // tdrStyle->SetPadBorderSize(Width_t size = 1);
   tdrStyle->SetPadColor(kWhite);
   tdrStyle->SetPadGridX(false);
   tdrStyle->SetPadGridY(false);
   tdrStyle->SetGridColor(0);
   tdrStyle->SetGridStyle(3);
   tdrStyle->SetGridWidth(1);
-
-// For the frame:
+  // For the frame:
   tdrStyle->SetFrameBorderMode(0);
   tdrStyle->SetFrameBorderSize(1);
   tdrStyle->SetFrameFillColor(0);
@@ -82,35 +81,22 @@ void setTDRStyle() {
   tdrStyle->SetFrameLineColor(1);
   tdrStyle->SetFrameLineStyle(1);
   tdrStyle->SetFrameLineWidth(1);
-
-// For the histo:
-  // tdrStyle->SetHistFillColor(1);
-  // tdrStyle->SetHistFillStyle(0);
+  // For the histo:
   tdrStyle->SetHistLineColor(1);
   tdrStyle->SetHistLineStyle(0);
   tdrStyle->SetHistLineWidth(1);
-  // tdrStyle->SetLegoInnerR(Float_t rad = 0.5);
-  // tdrStyle->SetNumberContours(Int_t number = 20);
-
   tdrStyle->SetEndErrorSize(2);
-  // tdrStyle->SetErrorMarker(20);
   tdrStyle->SetErrorX(0.);
-  
   tdrStyle->SetMarkerStyle(20);
-
-//For the fit/function:
+  //For the fit/function:
   tdrStyle->SetOptFit(1);
   tdrStyle->SetFitFormat("5.4g");
   tdrStyle->SetFuncColor(2);
   tdrStyle->SetFuncStyle(1);
   tdrStyle->SetFuncWidth(1);
-
-//For the date:
+  //For the date:
   tdrStyle->SetOptDate(0);
-  // tdrStyle->SetDateX(Float_t x = 0.01);
-  // tdrStyle->SetDateY(Float_t y = 0.01);
-
-// For the statistics box:
+  // For the statistics box:
   tdrStyle->SetOptFile(0);
   tdrStyle->SetOptStat(0); // To display the mean and RMS:   SetOptStat("mr");
   tdrStyle->SetStatColor(kWhite);
@@ -121,79 +107,44 @@ void setTDRStyle() {
   tdrStyle->SetStatBorderSize(1);
   tdrStyle->SetStatH(0.1);
   tdrStyle->SetStatW(0.15);
-  // tdrStyle->SetStatStyle(Style_t style = 1001);
-  // tdrStyle->SetStatX(Float_t x = 0);
-  // tdrStyle->SetStatY(Float_t y = 0);
-
-// Margins:
+  // Margins:
   tdrStyle->SetPadTopMargin(0.05);
   tdrStyle->SetPadBottomMargin(0.13);
   tdrStyle->SetPadLeftMargin(0.16);
   tdrStyle->SetPadRightMargin(0.02);
-
-// For the Global title:
-
+  // For the Global title:
   tdrStyle->SetOptTitle(0);
   tdrStyle->SetTitleFont(42);
   tdrStyle->SetTitleColor(1);
   tdrStyle->SetTitleTextColor(1);
   tdrStyle->SetTitleFillColor(10);
   tdrStyle->SetTitleFontSize(0.05);
-  // tdrStyle->SetTitleH(0); // Set the height of the title box
-  // tdrStyle->SetTitleW(0); // Set the width of the title box
-  // tdrStyle->SetTitleX(0); // Set the position of the title box
-  // tdrStyle->SetTitleY(0.985); // Set the position of the title box
-  // tdrStyle->SetTitleStyle(Style_t style = 1001);
-  // tdrStyle->SetTitleBorderSize(2);
-
-// For the axis titles:
-
+  // For the axis titles:
   tdrStyle->SetTitleColor(1, "XYZ");
   tdrStyle->SetTitleFont(42, "XYZ");
   tdrStyle->SetTitleSize(0.06, "XYZ");
-  // tdrStyle->SetTitleXSize(Float_t size = 0.02); // Another way to set the size?
-  // tdrStyle->SetTitleYSize(Float_t size = 0.02);
   tdrStyle->SetTitleXOffset(0.9);
-  tdrStyle->SetTitleYOffset(1.25);
-  // tdrStyle->SetTitleOffset(1.1, "Y"); // Another way to set the Offset
-
-// For the axis labels:
-
+  tdrStyle->SetTitleYOffset(1.5);
+  // For the axis labels:
   tdrStyle->SetLabelColor(1, "XYZ");
   tdrStyle->SetLabelFont(42, "XYZ");
   tdrStyle->SetLabelOffset(0.007, "XYZ");
   tdrStyle->SetLabelSize(0.05, "XYZ");
-
-// For the axis:
-
+  // For the axis:
   tdrStyle->SetAxisColor(1, "XYZ");
   tdrStyle->SetStripDecimals(kTRUE);
   tdrStyle->SetTickLength(0.03, "XYZ");
   tdrStyle->SetNdivisions(510, "XYZ");
   tdrStyle->SetPadTickX(1);  // To get tick marks on the opposite side of the frame
   tdrStyle->SetPadTickY(1);
-
-// Change for log plots:
+  // Change for log plots:
   tdrStyle->SetOptLogx(0);
   tdrStyle->SetOptLogy(0);
   tdrStyle->SetOptLogz(0);
-
-// Postscript options:
+  // Postscript options:
   tdrStyle->SetPaperSize(20.,20.);
-  // tdrStyle->SetLineScalePS(Float_t scale = 3);
-  // tdrStyle->SetLineStyleString(Int_t i, const char* text);
-  // tdrStyle->SetHeaderPS(const char* header);
-  // tdrStyle->SetTitlePS(const char* pstitle);
-
-  // tdrStyle->SetBarOffset(Float_t baroff = 0.5);
-  // tdrStyle->SetBarWidth(Float_t barwidth = 0.5);
-  // tdrStyle->SetPaintTextFormat(const char* format = "g");
-  // tdrStyle->SetPalette(Int_t ncolors = 0, Int_t* colors = 0);
-  // tdrStyle->SetTimeOffset(Double_t toffset);
-  // tdrStyle->SetHistMinimumZero(kTRUE);
-
+  // make active
   tdrStyle->cd();
-
 }
 
 int main(int argc, char *argv[]){
@@ -220,6 +171,7 @@ int main(int argc, char *argv[]){
   std::vector<TDirectory*> _filesMC;
   const edm::ParameterSet& parameters = builder.processDesc()->getProcessPSet()->getParameter<edm::ParameterSet>("CombinePlots");
   std::string outputFile = parameters.getParameter<std::string>("outputFile");
+  edm::ParameterSet options = parameters.getParameter<edm::ParameterSet>("options");
   std::vector<edm::ParameterSet> dataInputs = parameters.getParameter<std::vector<edm::ParameterSet> >("data");
   std::vector<edm::ParameterSet> mcInputs = parameters.getParameter<std::vector<edm::ParameterSet> >("mc");
   std::vector<edm::ParameterSet> styleTweaks = parameters.getParameter<std::vector<edm::ParameterSet> >("formating");
@@ -229,7 +181,7 @@ int main(int argc, char *argv[]){
   setTDRStyle();
   gROOT->SetBatch();  
   // do the job
-  plotCombiner combiner;
+  plotCombiner combiner(options);
   combiner.setDatacfg(dataInputs);
   combiner.setMCcfg(mcInputs);
   combiner.setStyleTweaks(styleTweaks);
@@ -242,14 +194,21 @@ int main(int argc, char *argv[]){
 
 void plotCombiner::CombineHistos(const char* name, std::vector<TDirectory*> datadirs, std::vector<TDirectory*> mcdirs, TDirectory* output)
 {
-std::cout << "plotCombiner::CombineHistos" << std::endl;
+   // we need something called data to initialize the canvas and stack.
+   // if no data is provided, copy first MC.
+   bool drawData = true;
+   if(!datadirs.size()) {
+     datadirs = mcdirs;
+     drawData = false;
+   }
+   // create everything, using also first data histo
    TH1* data;
    (*datadirs.begin())->GetObject(name,data);
    data = (TH1*) data->Clone();
-std::cout << "working on histo " << data->GetName() << std::endl;
    TCanvas* c = new TCanvas(data->GetName(),data->GetTitle());
    THStack* stack = new THStack(data->GetName(),"MC stack");
    TLegend* leg = new TLegend(0.6,0.7,0.9,0.9);
+   // DATA
    std::map<std::string, edm::ParameterSet>::iterator style = _styleTweaks.find(data->GetName());
    for(std::vector<TDirectory*>::const_iterator it = datadirs.begin()+1;it<datadirs.end();++it) {
      TH1* h;
@@ -259,6 +218,7 @@ std::cout << "working on histo " << data->GetName() << std::endl;
    if(style!=_styleTweaks.end()) data->Rebin(style->second.getUntrackedParameter<unsigned>("rebin",1));
    data->SetTitle("data");
    leg->AddEntry(data,"Data","LE");
+   // now to the MC
    std::vector<edm::ParameterSet>::const_iterator mcConf = _mcInputs.begin();
    for(std::vector<TDirectory*>::const_iterator it = mcdirs.begin();it<mcdirs.end();++it, ++mcConf) {
      TH1* h;
@@ -267,20 +227,58 @@ std::cout << "working on histo " << data->GetName() << std::endl;
      h->SetTitle(mcConf->getParameter<std::string>("role").c_str());
      // color
      h->SetFillColor(mcConf->getParameter<unsigned int>("color"));
+     //h->SetLineColor(mcConf->getParameter<unsigned int>("color"));
      // scale
-     h->SumW2();
+     h->Sumw2();
      h->Scale(mcConf->getParameter<double>("scale"));
      // rebin
-     if(style!=_styleTweaks.end()) h->Rebin(style->second.getUntrackedParameter<unsigned>("rebin",1));
+     if(style!=_styleTweaks.end()) {
+       // simple rebin
+       unsigned ngroup = style->second.getUntrackedParameter<unsigned>("rebin",1);
+       // rebin using an explicit binning
+       std::vector<double> bins = style->second.getUntrackedParameter<std::vector<double> >("bins",std::vector<double>() );
+       // rebin using begin, end, width
+       double begin = style->second.getUntrackedParameter<double>("begin",0);
+       double end   = style->second.getUntrackedParameter<double>("end",-1);
+       double width = style->second.getUntrackedParameter<double>("width",1);
+       // now, prepare the array of bins
+       if(end>begin && bins.size()==0) {
+         width = width>h->GetBinWidth(h->GetNbinsX()/2) ? width : h->GetBinWidth(h->GetNbinsX()/2) ;
+         for(double x=begin;x<end;x+=width)
+	   bins.push_back(x);
+         bins.push_back(end);
+       }
+       // actually do the rebinning
+       if(bins.size()) {
+         double* xbins = new double[bins.size()];
+	 for(unsigned i=0;i<bins.size();++i)
+	   xbins[i] = bins[i];
+	 ngroup = bins.size()-1;
+         h->Rebin(ngroup,"",xbins);
+	 delete[] xbins;
+       } else {
+         h->Rebin(ngroup);
+       }
+     }
      // add
      stack->Add(h);
      // legend
      leg->AddEntry(h,mcConf->getParameter<std::string>("role").c_str(),"f");
    }
-   data->Draw("e");         // first draw data... that fixes the scale
-   stack->Draw("same");     // then the stack on top
-   data->Draw("e, same");   // and again data to see all points
-   leg->Draw();
+   if(drawData) {
+     data->Draw("e");         // first draw data... that fixes the scale
+     if(_nostack)
+       stack->Draw("nostack, hist, same");     // then the stack on top
+     else
+       stack->Draw("hist, same");   // then the stack on top
+     data->Draw("e, same");   // and again data to see all points
+   } else {
+     if(_nostack)
+       stack->Draw("nostack");// draw the mc alone
+     else
+       stack->Draw();         // draw the mc alone
+   }
+   leg->Draw();               // draw the legend
    if(style!=_styleTweaks.end()) {
      // we might add in the config file a set of entries to set labels, axis, etc.
      // everything is done using untracked parameters, so it is easy and quick to add things.
@@ -297,8 +295,11 @@ std::cout << "working on histo " << data->GetName() << std::endl;
 
 void plotCombiner::CombineDir(std::vector<TDirectory*> datadirs, std::vector<TDirectory*> mcdirs, TDirectory* output)
 {
-   TDirectory* datadir = datadirs[0];
-   TList* keys = datadir->GetListOfKeys();
+   TDirectory* refdir = NULL;
+   if(datadirs.size()) refdir = datadirs[0];
+   else if(mcdirs.size()) refdir = mcdirs[0];
+   else return;
+   TList* keys = refdir->GetListOfKeys();
    TIter next(keys);
    TKey *key;
    while ((key = (TKey*) next())) {
