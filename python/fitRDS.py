1876d8e96eafac782895272b33106df81ec74127
Tristan du Pree <tristan.dupree@uclouvain.be>
2011-05-22 18:24:30 +0000

    Added some 1st versions of possible stat methods

--- python/fitRDS.py
+++ python/fitRDS.py
@@ -22,7 +22,7 @@ gROOT.SetStyle("Plain")
 
 ### GET DATA ###
 
-f_Zbb = TFile("File_rds_zbb_mu.root")
+f_Zbb = TFile("File_rds_zbb_el.root")
 ws_Zbb = f_Zbb.Get("ws")
 rds_zbb  = ws_Zbb.data("rds_zbb")
 rrv_SV_M  = ws_Zbb.var("rrv_SV_M")
@@ -47,7 +47,9 @@ print "#Events level 8 = ", rds_8.numEntries()
 print "#Events level 9 = ", rds_9.numEntries() 
 print "############################"
 
+#########################
 ### GET BACKGROUND MC ###
+#########################
 
 f_zbb_MC    = TFile("File_rds_zbb_muMC.root")
 ws_zbb_MC   = f_zbb_MC.Get("ws")
@@ -56,15 +58,18 @@ rds_zbb_MC  = ws_zbb_MC.data("rds_zbb")
 rds_zbb_MC_8 = rds_zbb_MC.reduce("rc_cat>=8") #ZbbHPHE
 rds_zbb_MC_9 = rds_zbb_MC.reduce("rc_cat>=9") #ZbbHPHP
 
+#########################
 ### MAKE SIGNAL PDF ### (analytic or template)
+#########################
 
 mean_bb  = RooRealVar("mean_bb", "mean_bb", 300,100,500)
 sigma_bb = RooRealVar("sigma_bb","sigma_bb", 50, 10,100)
 
 sig_bb = RooGaussian("sig_bb","sig_bb",rrv_bb_M,mean_bb,sigma_bb)
 
+#########################
 ### MAKE BACKGROUND PDF ### (analytic or template)
-
+#########################
 
 # RooKeysPdf* myRHP_b = new RooKeysPdf("myRHP_b","myRHP_b",*SV_m,*RDS_Zbb);
 # RooKeysPdf* myRHP_c = new RooKeysPdf("myRHP_c","myRHP_c",*SV_m,*RDS_Zcc);
@@ -83,6 +88,10 @@ rhp_zbb_MC_8_bb_M = RooHistPdf( "rhp_zbb_MC_8_bb_M", "rhp_zbb_MC_8_bb_M", RooArg
 bkg_bb = rhp_zbb_MC_8_bb_M
 bkg_bb.SetName("bkg_bb")
 
+######################
+### MAKE TOTAL PDF ###
+######################
+
 S_8=RooRealVar("S_8","S_8",0.8*rds_zbb_MC_8.numEntries(),0,rds_zbb_MC_8.numEntries())
 B_8=RooRealVar("B_8","B_8",0.2*rds_zbb_MC_8.numEntries(),0,rds_zbb_MC_8.numEntries())
 
@@ -98,9 +107,112 @@ frame.Draw()
 
 bla
 
+###########
+### FIT ###
+###########
+
 #sum_bb.fitTo(rds_8,RooFit.Extended())
 
+### more statistical methods? see http://root.cern.ch/root/html/tutorials/roostats/index.html
+
+##########################
+### PROFILE LIKELIHOOD ###
+##########################
+
+### see also http://root.cern.ch/root/html/tutorials/roostats/StandardProfileLikelihoodDemo.C.html
+
+################################
+### Make likelihood profiles ###
+################################
+
+# can do 2D (see below)
+# 1D (number of signal events) should be sufficient
+
+print "likelihood of Phi_s alone"
+
+plc_Phi_s = RooStats.ProfileLikelihoodCalculator(data,myJpsiphiPdf,RooArgSet(Sf_s))
+plot_Phi_s = RooStats.LikelihoodIntervalPlot(plc_Phi_s.GetInterval())
+
+print "likelihood of dGG_s alone"
+
+plc_dGG_s = RooStats.ProfileLikelihoodCalculator(data,myJpsiphiPdf,RooArgSet(dGG_s))
+plot_dGG_s = RooStats.LikelihoodIntervalPlot(plc_dGG_s.GetInterval())
+
+print "plotting likelihood of (dGG_s,Phi_s)"
+
+print " ***************"
+print " *** 3 sigma ***"
+print " ***************"
+plc_2D_99 = RooStats.ProfileLikelihoodCalculator(data,myJpsiphiPdf,RooArgSet(Phi_s,dGG_s))
+plc_2D_99.SetTestSize(0.01)
+plot_2D_99 = RooStats.LikelihoodIntervalPlot(plc_2D_99.GetInterval())
+print " ***************"
+print " *** 2 sigma ***"
+print " ***************"
+plc_2D_95 = RooStats.ProfileLikelihoodCalculator(data,myJpsiphiPdf,RooArgSet(Phi_s,dGG_s))
+plc_2D_95.SetTestSize(0.05)
+plot_2D_95 = RooStats.LikelihoodIntervalPlot(plc_2D_95.GetInterval())
+print " ***************"
+print " *** 1 sigma ***"
+print " ***************"
+plc_2D_68 = RooStats.ProfileLikelihoodCalculator(data,myJpsiphiPdf,RooArgSet(Phi_s,dGG_s))
+plc_2D_68.SetTestSize(0.32)
+plot_2D_68 = RooStats.LikelihoodIntervalPlot(plc_2D_68.GetInterval())
+
+#"nominuit" option avoids:
+#(1) possible problems in minimization using minuit: instead of looking for contour it scans whole 2D space
+#(2) bug in axis labelling
+
+C=TCanvas("C","C",1200,350)
+C.Divide(3)
+
+print "plotting likelihood of Phi_s alone"
+C.cd(1)
+#plot_Phi_s.Draw()
+print "plotting likelihood of dGG_s alone"
+C.cd(2)
+#plot_dGG_s.Draw()
+
+print "plotting likelihood of (dGG_s,Phi_s)"
+C.cd(3)
+print " ***************"
+print " *** 3 sigma ***"
+print " ***************"
+plot_2D_99.SetContourColor(RooFit.kYellow)
+#plot_2D_99.SetPrecision(0.001)# I don't understand this option
+plot_2D_99.SetNPoints(10)
+plot_2D_99.Draw("nominuit")
+print " ***************"
+print " *** 2 sigma ***"
+print " ***************"
+plot_2D_95.SetContourColor(RooFit.kOrange)
+plot_2D_95.SetNPoints(10)
+plot_2D_95.Draw("nominuit,same")
+print " ***************"
+print " *** 1 sigma ***"
+print " ***************"
+#plot_2D_68.SetContourColor(RooFit.kRed)
+#plot_2D_68.SetNPoints(10)
+#plot_2D_68.Draw("nominuit,same")
+
+
+
+C.SaveAs("profLikelihood.eps")
+
+print "Finished plotting"
+
+
+
+################
+### CLs SCAN ###
+################
+
+### http://root.cern.ch/root/html/tutorials/roostats/HybridInstructional.C.html
+
+############
 ### PLOT ###
+############
+
 
 C4=TCanvas("C4","C4",900,900)
 C4.Divide(2,2)
