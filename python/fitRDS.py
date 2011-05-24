108e9194c97a403e38428c1b4bebbda59ca69e2a
Tristan du Pree <tristan.dupree@uclouvain.be>
2011-05-24 18:45:53 +0000

    Cleaned up a bit.

--- python/fitRDS.py
+++ python/fitRDS.py
@@ -4,14 +4,20 @@
 ### fitRDS.py (TdP)                                                                ### 
 ###                                                                                ###  
 ### Analyze the RooDataSet produced by makeRDS.py to perform a Higgs search        ###
+###                                                                                ### 
+### Functionality:                                                                 ###
+### - Fit for signal peak                                                          ###
+### - Estimate significance sigal peak using profile likelihood                    ###
+### - CLs scan using different models as function of mH hypothesis                 ###                          
 ###                                                                                ###
 ### To do:                                                                         ###
-### - check difference reco composite candidates (e.g. zbb vs home-brewn)          ###
-### - get proper MC sample                                                         ###
+### - get proper MC samples                                                        ###
 ### - allow for options to use both template and functional PDFs                   ###
 ### - factorize data-getting/pdf-making/fitting/plotting/etc...a bit less ugly ;-) ###
-### - make profile likelihoods etc (using RooStats)                                ### 
-###                                                                                ### 
+### - check CLs scan (and do the plot of the scan more elegantly)                  ###
+###                                                                                ###
+### more statistical methods? see http://root.cern.ch/root/html/tutorials/roostats/index.html
+###                                                                                ###    
 ######################################################################################
 ######################################################################################
 
@@ -20,7 +26,9 @@ from ROOT import *
 
 gROOT.SetStyle("Plain")
 
+################
 ### GET DATA ###
+################
 
 f_Zbb = TFile("File_rds_zbb_el.root")
 ws_Zbb = f_Zbb.Get("ws")
@@ -67,25 +75,18 @@ rds_zbb_MC  = ws_zbb_MC.data("rds_zbb")
 rds_zbb_MC_8 = rds_zbb_MC.reduce("rc_cat>=8") #ZbbHPHE
 rds_zbb_MC_9 = rds_zbb_MC.reduce("rc_cat>=9") #ZbbHPHP
 
-#########################
-### MAKE SIGNAL PDF ### (analytic or template)
-#########################
+##############################################
+### MAKE SIGNAL PDF (analytic or template) ###
+##############################################
 
 mean_bb  = RooRealVar("mean_bb", "mean_bb", 300,100,500)
 sigma_bb = RooRealVar("sigma_bb","sigma_bb", 30, 10,100)
 
 Sig_bb = RooGaussian("Sig_bb","Sig_bb",rrv_bb_M,mean_bb,sigma_bb)
 
-#sig_bb_300 = RooGaussian("sig_bb_300","sig_bb_300",rrv_bb_M,RooFit.RooConst(300),RooFit.RooConst(25))
-#sig_bb_400 = RooGaussian("sig_bb_400","sig_bb_400",rrv_bb_M,RooFit.RooConst(400),RooFit.RooConst(25))
-
-#########################
-### MAKE BACKGROUND PDF ### (analytic or template)
-#########################
-
-# RooKeysPdf* myRHP_b = new RooKeysPdf("myRHP_b","myRHP_b",*SV_m,*RDS_Zbb);
-# RooKeysPdf* myRHP_c = new RooKeysPdf("myRHP_c","myRHP_c",*SV_m,*RDS_Zcc);
-# RooKeysPdf* myRHP_l = new RooKeysPdf("myRHP_l","myRHP_l",*SV_m,*RDS_Zll);
+##################################################
+### MAKE BACKGROUND PDF (analytic or template) ###
+##################################################
 
 rdh_zbb_MC_8_SV_M = RooDataHist("rdh_zbb_MC_8_SV_M", "rdh_zbb_MC_8_SV_M", RooArgSet(rrv_SV_M), rds_zbb_MC_8)
 rdh_zbb_MC_8_bb_M = RooDataHist("rdh_zbb_MC_8_bb_M", "rdh_zbb_MC_8_bb_M", RooArgSet(rrv_bb_M), rds_zbb_MC_8)
@@ -93,13 +94,6 @@ rhp_zbb_MC_8_SV_M = RooHistPdf( "rhp_zbb_MC_8_SV_M", "rhp_zbb_MC_8_SV_M", RooArg
 rhp_zbb_MC_8_bb_M = RooHistPdf( "rhp_zbb_MC_8_bb_M", "rhp_zbb_MC_8_bb_M", RooArgSet(rrv_bb_M), rdh_zbb_MC_8_bb_M);
 
 rhp_zbb_MC_8_bb_M = RooKeysPdf("rhp_zbb_MC_8_bb_M","rhp_zbb_MC_8_bb_M", rrv_bb_M, rds_zbb_MC_8)
-
-#RooDataHist* myRDH_c = new RooDataHist("myZc", "myRDH_Zc", RooArgSet(*SV_m), *RDS_Zcc);
-#RooDataHist* myRDH_l = new RooDataHist("myZl", "myRDH_Zl", RooArgSet(*SV_m), *RDS_Zll);
-#RooHistPdf* myRHP_c = new RooHistPdf("myRHP_c","myRHP_c",RooArgSet(*SV_m),*myRDH_c);
-#RooHistPdf* myRHP_l = new RooHistPdf("myRHP_l","myRHP_l",RooArgSet(*SV_m),*myRDH_l);
-
-#rhp_zbb_MC_8_bb_M
 
 #bkg_yield = RooRealVar("bkg_yield","",25,0,300);
 B_8=RooRealVar("B_8","B_8",0.2*rds_8.numEntries(),0,rds_8.numEntries())
@@ -115,10 +109,11 @@ S_8=RooRealVar("S_8","N_{S}",0.8*rds_8.numEntries(),0,rds_8.numEntries())
 
 Sum_bb = RooAddPdf("Sum_bb","Sum_bb",RooArgList(Sig_bb,bkg_bb),RooArgList(S_8,B_8))
 
+###########
+### FIT ###
+###########
 
-#sum_bb_200 = RooAddPdf("sum_bb_200","sum_bb_200",RooArgList(sig_bb_200,bkg_bb),RooArgList(S_8,B_8))
-#sum_bb_300 = RooAddPdf("sum_bb_300","sum_bb_300",RooArgList(sig_bb_300,bkg_bb),RooArgList(S_8,B_8))
-#sum_bb_400 = RooAddPdf("sum_bb_400","sum_bb_400",RooArgList(sig_bb_400,bkg_bb),RooArgList(S_8,B_8))
+### more statistical methods? see http://root.cern.ch/root/html/tutorials/roostats/index.html
 
 Sum_bb.fitTo(rds_8,RooFit.Extended())
 
@@ -128,16 +123,9 @@ Sum_bb.plotOn(frame,RooFit.LineColor(kBlue))
 Sum_bb.plotOn(frame,RooFit.Components("Sig_bb"),RooFit.LineColor(kGreen),RooFit.LineStyle(kDashed))
 Sum_bb.plotOn(frame,RooFit.Components("bkg_bb"),RooFit.LineColor(kRed),  RooFit.LineStyle(kDashed))
 Sum_bb.paramOn(frame,rds_8)
+
+C_fit=TCanvas("C_fit","C_fit",800,700)
 frame.Draw()
-
-
-###########
-### FIT ###
-###########
-
-#sum_bb.fitTo(rds_8,RooFit.Extended())
-
-### more statistical methods? see http://root.cern.ch/root/html/tutorials/roostats/index.html
 
 ##########################
 ### PROFILE LIKELIHOOD ###
@@ -145,19 +133,15 @@ frame.Draw()
 
 ### see also http://root.cern.ch/root/html/tutorials/roostats/StandardProfileLikelihoodDemo.C.html
 
-################################
-### Make likelihood profiles ###
-################################
-
-# can do 2D (see below)
+# can also do 2D 
 # 1D (number of signal events) should be sufficient
 
 plc_S = RooStats.ProfileLikelihoodCalculator(rds_8,Sum_bb,RooArgSet(S_8))
 plot_S = RooStats.LikelihoodIntervalPlot(plc_S.GetInterval())
 
-C=TCanvas("C","C",400,350)
-
 print "plotting likelihood of S alone"
+
+C_profLik=TCanvas("C_profLik","C_profLik",400,350)
 plot_S.Draw()
 
 ###############
@@ -224,10 +208,10 @@ for m in mList :
     myHybridResult[m].SetDataTestStatistics(mean_sb_toys_test_stat[m])
     toys_significance[m] = myHybridResult[m].Significance()
 
+print "***Completed HybridCalculatorOriginal example"
+print "*** varying Higgs mass hypothesis and estimating confidence level ***"
 for m in mList :
-    print "Completed HybridCalculatorOriginal example"
-    print "*** floating Higgs mass ***"
-    print "*** m(Higgs)= ", m , " ***"
+    print "==> m(Higgs) = ", m ," <==="
     print " - -2lnQ = " , min2lnQ_data[m] 
     print " - CL_sb = " , clsb_data[m]
     print " - CL_b  = " , clb_data[m]
@@ -278,208 +262,8 @@ myTH1_cls_plus.SetFillColor(kYellow)
 myTH1_cls_min.SetFillColor(kGreen)
 myTH1_cls_2min.SetFillColor(1001)
 
-bla
+##################
+### THE END :( ###
+##################
 
-### save the toy-MC results to file, this way splitting into sub-batch jobs is possible
-###TFile fileOut("some_hybridresult.root","RECREATE");
-###fileOut.cd();
-###myHybridResult.Write();
-##fileOut.Close();
-
-### read the results from a file
-##TFile fileIn("some_hybridresult.root");
-##HybridResult* myOtherHybridResult = (HybridResult*) fileIn.Get("myHybridCalc");
-
-### example on how to merge with toy-MC results obtained in another job
-##HybridResult* mergedHybridResult = new HybridResult("mergedHybridResult","this object holds merged results");
-##mergedHybridResult->Add(myHybridResult);
-##mergedHybridResult->Add(myOtherHybridResult);
-## or
-##myHybridResult->Add(myOtherHybridResult);
-
-
-
-print "likelihood of dGG_s alone"
-
-plc_dGG_s = RooStats.ProfileLikelihoodCalculator(data,myJpsiphiPdf,RooArgSet(dGG_s))
-plot_dGG_s = RooStats.LikelihoodIntervalPlot(plc_dGG_s.GetInterval())
-
-print "plotting likelihood of (dGG_s,Phi_s)"
-
-print " ***************"
-print " *** 3 sigma ***"
-print " ***************"
-plc_2D_99 = RooStats.ProfileLikelihoodCalculator(data,myJpsiphiPdf,RooArgSet(Phi_s,dGG_s))
-plc_2D_99.SetTestSize(0.01)
-plot_2D_99 = RooStats.LikelihoodIntervalPlot(plc_2D_99.GetInterval())
-print " ***************"
-print " *** 2 sigma ***"
-print " ***************"
-plc_2D_95 = RooStats.ProfileLikelihoodCalculator(data,myJpsiphiPdf,RooArgSet(Phi_s,dGG_s))
-plc_2D_95.SetTestSize(0.05)
-plot_2D_95 = RooStats.LikelihoodIntervalPlot(plc_2D_95.GetInterval())
-print " ***************"
-print " *** 1 sigma ***"
-print " ***************"
-plc_2D_68 = RooStats.ProfileLikelihoodCalculator(data,myJpsiphiPdf,RooArgSet(Phi_s,dGG_s))
-plc_2D_68.SetTestSize(0.32)
-plot_2D_68 = RooStats.LikelihoodIntervalPlot(plc_2D_68.GetInterval())
-
-#"nominuit" option avoids:
-#(1) possible problems in minimization using minuit: instead of looking for contour it scans whole 2D space
-#(2) bug in axis labelling
-
-C=TCanvas("C","C",1200,350)
-C.Divide(3)
-
-print "plotting likelihood of Phi_s alone"
-C.cd(1)
-#plot_Phi_s.Draw()
-print "plotting likelihood of dGG_s alone"
-C.cd(2)
-#plot_dGG_s.Draw()
-
-print "plotting likelihood of (dGG_s,Phi_s)"
-C.cd(3)
-print " ***************"
-print " *** 3 sigma ***"
-print " ***************"
-plot_2D_99.SetContourColor(RooFit.kYellow)
-#plot_2D_99.SetPrecision(0.001)# I don't understand this option
-plot_2D_99.SetNPoints(10)
-plot_2D_99.Draw("nominuit")
-print " ***************"
-print " *** 2 sigma ***"
-print " ***************"
-plot_2D_95.SetContourColor(RooFit.kOrange)
-plot_2D_95.SetNPoints(10)
-plot_2D_95.Draw("nominuit,same")
-print " ***************"
-print " *** 1 sigma ***"
-print " ***************"
-#plot_2D_68.SetContourColor(RooFit.kRed)
-#plot_2D_68.SetNPoints(10)
-#plot_2D_68.Draw("nominuit,same")
-
-
-
-C.SaveAs("profLikelihood.eps")
-
-print "Finished plotting"
-
-
-
-################
-### CLs SCAN ###
-################
-
-### http://root.cern.ch/root/html/tutorials/roostats/HybridInstructional.C.html
-
-############
-### PLOT ###
-############
-
-
-C4=TCanvas("C4","C4",900,900)
-C4.Divide(2,2)
-
-C4.cd(1)
-frame_SV_M_4 = rrv_SV_M.frame(0,rrv_SV_M.getMax())
-rds_4.plotOn(frame_SV_M_4)
-frame_SV_M_4.Draw()
-
-C4.cd(2)
-frame_bb_M_4 = rrv_bb_M.frame(0,rrv_bb_M.getMax())
-rds_4.plotOn(frame_bb_M_4)
-frame_bb_M_4.Draw()
-
-C4.cd(3)
-frame_zeebb_M_4 = rrv_zeebb_M.frame(0,rrv_zeebb_M.getMax())
-rds_4.plotOn(frame_zeebb_M_4)
-frame_zeebb_M_4.Draw()
-
-C4.cd(4)
-frame_zmmbb_M_4 = rrv_zmmbb_M.frame(0,rrv_zmmbb_M.getMax())
-rds_4.plotOn(frame_zmmbb_M_4)
-frame_zmmbb_M_4.Draw()
-
-
-C5=TCanvas("C5","C5",900,900)
-C5.Divide(2,2)
-
-C5.cd(1)
-frame_SV_M_5 = rrv_SV_M.frame(0,rrv_SV_M.getMax())
-rds_5.plotOn(frame_SV_M_5)
-frame_SV_M_5.Draw()
-
-C5.cd(2)
-frame_bb_M_5 = rrv_bb_M.frame(0,rrv_bb_M.getMax())
-rds_5.plotOn(frame_bb_M_5)
-frame_bb_M_5.Draw()
-
-C4.cd(3)
-frame_zeebb_M_5 = rrv_zeebb_M.frame(0,rrv_zeebb_M.getMax())
-rds_5.plotOn(frame_zeebb_M_5)
-frame_zeebb_M_5.Draw()
-
-C4.cd(4)
-frame_zmmbb_M_5 = rrv_zmmbb_M.frame(0,rrv_zmmbb_M.getMax())
-rds_5.plotOn(frame_zmmbb_M_5)
-frame_zmmbb_M_5.Draw()
-
-
-C8=TCanvas("C8","C8",900,900)
-C8.Divide(2,2)
-
-C8.cd(1)
-frame_SV_M_8 = rrv_SV_M.frame(0,rrv_SV_M.getMax())
-rds_8.plotOn(frame_SV_M_8)
-frame_SV_M_8.Draw()
-
-C8.cd(2)
-frame_bb_M_8 = rrv_bb_M.frame(0,rrv_bb_M.getMax())
-rds_8.plotOn(frame_bb_M_8)
-frame_bb_M_8.Draw()
-
-C8.cd(3)
-frame_zeebb_M_8 = rrv_zeebb_M.frame(0,rrv_zeebb_M.getMax())
-rds_8.plotOn(frame_zeebb_M_8)
-frame_zeebb_M_8.Draw()
-
-C8.cd(4)
-frame_zmmbb_M_8 = rrv_zmmbb_M.frame(0,rrv_zmmbb_M.getMax())
-rds_8.plotOn(frame_zmmbb_M_8)
-frame_zmmbb_M_8.Draw()
-
-
-C9=TCanvas("C9","C9",900,900)
-C9.Divide(3)
-
-C9.cd(1)
-frame_SV_M_9 = rrv_SV_M.frame(0,rrv_SV_M.getMax())
-rds_9.plotOn(frame_SV_M_9)
-frame_SV_M_9.Draw()
-
-C9.cd(2)
-frame_bb_M_9 = rrv_bb_M.frame(0,rrv_bb_M.getMax())
-rds_9.plotOn(frame_bb_M_9)
-frame_bb_M_9.Draw()
-
-C9.cd(3)
-frame_zeebb_M_9 = rrv_zeebb_M.frame(0,rrv_zeebb_M.getMax())
-rds_9.plotOn(frame_zeebb_M_9)
-frame_zeebb_M_9.Draw()
-
-C9.cd(4)
-frame_zmmbb_M_9 = rrv_zmmbb_M.frame(0,rrv_zmmbb_M.getMax())
-rds_9.plotOn(frame_zmmbb_M_9)
-frame_zmmbb_M_9.Draw()
-
-#data     = sum_bb.generate(RooArgSet(rrv_bb_M),RooFit.Extended())
-#data_200 = sum_bb_200.generate(RooArgSet(rrv_bb_M),RooFit.Extended())
-#data_300 = sum_bb_300.generate(RooArgSet(rrv_bb_M),RooFit.Extended())
-#data_400 = sum_bb_400.generate(RooArgSet(rrv_bb_M),RooFit.Extended())
-#myHybridCalc     = RooStats.HybridCalculatorOriginal(data, sum_bb , bkg_bb ,RooArgSet())
-#myHybridCalc_200 = RooStats.HybridCalculatorOriginal(data_200, sum_bb_200 , bkg_bb ,RooArgSet())
-#myHybridCalc_300 = RooStats.HybridCalculatorOriginal(data_300, sum_bb_300 , bkg_bb ,RooArgSet())
-#myHybridCalc_400 = RooStats.HybridCalculatorOriginal(data_400, sum_bb_400 , bkg_bb ,RooArgSet())
+### more statistical methods? see http://root.cern.ch/root/html/tutorials/roostats/index.html
