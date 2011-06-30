d8fe29099f09b99774d31371382f14429c849b85
Ludivine Ceard <Ludivine.Ceard@cern.ch>
2011-06-30 17:54:44 +0000

    job spliting option

--- python/Launch.py
+++ python/Launch.py
@@ -8,10 +8,11 @@ import LaunchOnCondor
 import glob
 
 FarmDirectory = "FARM"
-JobName = "ZmumubbAnalysis"
+JobName = "ZmumubbAnalysis_MC_TTJets_Spring11"
 LaunchOnCondor.Jobs_RunHere = 1
 LaunchOnCondor.SendCluster_Create(FarmDirectory, JobName)
-for i in range(0,161):
-    LaunchOnCondor.SendCluster_Push(["PYTHON", os.getcwd()+"../ControlPlots_withSubJobs.py -i /home/fynu/lceard/store/Prod_AOD_2011A/Json_Golden_153pb/Mu_2011A_153pb/ -o Mu/Mu2011_153pb_"+str(i)+".root --all -n "+str(i)]) 
+LaunchOnCondor.Jobs_RunHere= 1
+for i in range(0,50):
+    LaunchOnCondor.SendCluster_Push(["PYTHON", os.getcwd()+"/ControlPlots.py -i /home/fynu/lceard/store/MC_Spring11/TTJets/ -o Control_Plots/MC_Spring11/TTJets_Spring11_Condor/TTJets_Spring11_"+str(i)+".root --all --noPU --Njobs 50 --jobNumber "+str(i)])
 LaunchOnCondor.SendCluster_Submit()
 
