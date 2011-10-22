f484bad2289d4b269a2ef280518668ce30f0b23b
Ludivine Ceard <Ludivine.Ceard@cern.ch>
2011-10-22 10:33:40 +0000

    some fixes but still need further debugging

--- python/JetCorrectionUncertainty.py
+++ python/JetCorrectionUncertainty.py
@@ -1,5 +1,8 @@
 import os
-AutoLibraryLoader.enable()
+import ROOT
+from math import sqrt
+ROOT.gSystem.Load("libFWCoreFWLite.so")
+ROOT.AutoLibraryLoader.enable()
 ROOT.gSystem.Load("libCondFormatsJetMETObjects.so")
 
 class JetCorrectionUncertaintyProxy:
@@ -10,16 +13,16 @@ class JetCorrectionUncertaintyProxy:
       print "Error: ",file, ": No such file."
       self._engine = None
     else:
-      self._engine = JetCorrectionUncertainty(file)
+      self._engine = ROOT.JetCorrectionUncertainty(file)
 
-  def unc_tot_jet(jet):
+  def unc_tot_jet(self,jet):
     if self._engine is None: return 0
     self._engine.setJetEta(jet.eta())# Give rapidity of jet you want uncertainty on
     self._engine.setJetPt(jet.pt()) #Also give the corrected pt of the jet you want the uncertainty on
     unc = self._engine.getUncertainty(1)
 
     if 0 <abs(jet.eta())< 1.5 : unc_dep_eta=0.02
-    if 1.5 <=abs(jet.eta())<3.0  : unc_dep_eta=0.06
+    if 1.5 <=abs(jet.eta())< 3.0  : unc_dep_eta=0.06
     if 3.0 <=abs(jet.eta()) : unc_dep_eta=0.20
 
     unc_tot=sqrt(pow(unc,2)+pow(0.015,2)+pow((0.2*0.8*2.2*1/jet.pt()),2)+pow(unc_dep_eta,2))
