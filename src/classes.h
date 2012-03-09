f788316ca0f682aac17c4e2d4c495a375d31b6f5
Christophe Delaere <christophe.delaere@cern.ch>
2012-03-09 09:57:47 +0000

    This commit was manufactured by cvs2git to create tag 'PAS-12-003'.
    Sprout from master 2012-03-09 09:57:46 UTC Ludivine Ceard <Ludivine.Ceard@cern.ch> 'to avoid danger'
    Delete:
        bin/BuildFile.xml
        bin/combinePlots.cc
        data/Jec10V1_Uncertainty_KT4PF.txt
        data/Pileup_2011_to_173692_CD111018.root
        data/Summer11_PU_S4_spikesmear.root
        data/performance_ssv_witheff.root
        interface/BTagWeight.h
        interface/btagPerfFWLiteInterface.h
        plugins/BuildFile.xml
        plugins/pileupDistInMC.cc
        scripts/DrawCanvas.C
        scripts/diow.C
        scripts/diow.pl
        scripts/publish.C
        scripts/skimPlots.C
        scripts/sumChannels.C
        scripts/tdrstyle.C
        scripts/testShape.C
        scripts/vertexAssociationFit.C
        scripts/yield.C
        src/BTagWeight.cpp
        src/BuildFile.xml
        src/WeightFromTrigger.cc
        src/btagPerfFWLiteInterface.C
        src/classes.h
        src/classes_def.xml
        test/combinePlots_Zj.py
        test/combinePlots_ZjZcZb_MCFM.py
        test/crab.cfg
        test/crab_template.cfg
        test/fireworks_PAT_Zbb.fwc
        test/multicrab.cfg
        test/patTuple_llbb_413_data_cfg.py
        test/patTuple_llbb_423_MC_cfg.py
        test/patTuple_llbb_423_data_cfg.py
        test/patTuple_llbb_MC_cfg.py
        test/patTuple_llbb_data_cfg.py
        test/patTuple_zbb_cfg.py
        test/patTuple_zbb_cfg_413.py
        test/patTuple_zbb_cfg_MC.py
        test/patTuple_zbb_cfg_specialevents.py
        test/skimDY_423_cfg.py
        test/testcombinePlots.py
        test/weightfromtrigger_cfg.py

--- src/classes.h
+++
@@ -1,5 +0,0 @@
-#include "DataFormats/Common/interface/Wrapper.h"
-
-//Add includes for your classes here
-#include "UserCode/zbb_louvain/interface/btagPerfFWLiteInterface.h"
-#include "UserCode/zbb_louvain/interface/BTagWeight.h"
