45af813a385619070b03f61be4b377187f123fba
Jesus Vizan <Jesus.Manuel.Vizan.Garcia@cern.ch>
2012-05-23 10:03:46 +0000

    Code to produce RDS files for all the samples for the 4_2 2.2fb llbb analysis (plus SM ZH with mH=125GeV. It can be easily adapted to run over different samples. It produces 1 condor job per process. Maybe there is hardcoded the macro with the script to run. A relative path should be used instead

---
+++ python/Launch_makeRDS.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+### to be run as
+#python Launch_makeRDS.py
+#it runs 1 job per each process specified in the variable processesToRun
+
+import urllib
+import string
+import os
+import sys
+import LaunchOnCondor
+import glob
+
+from optparse import OptionParser
+
+FarmDirectory = "FARM"
+
+processesToRun = ["Mu_DATA", "El_DATA", "Mu_MC", "El_MC", "Ttbar_Mu_MC", "Ttbar_El_MC", "ZZ_Mu_MC", "ZZ_El_MC", "ZHbb_Mu_MC", "ZHbb_El_MC"]
+print "I will run over ", len(processesToRun), "processes"
+
+
+JobName = "File_rds"
+LaunchOnCondor.Jobs_RunHere = 1
+LaunchOnCondor.SendCluster_Create(FarmDirectory, JobName)
+LaunchOnCondor.Jobs_RunHere= 1
+
+command = "/home/fynu/vizangarciaj/CMSSW_4_2_7/src/UserCode/zbb_louvain/python/makeRDS_usingFramework_batch.py"  
+print "command = ", command
+
+
+
+for x in processesToRun[:]:
+
+  #
+  #option  = " " + x
+
+  command = "/home/fynu/vizangarciaj/CMSSW_4_2_7/src/UserCode/zbb_louvain/python/makeRDS_usingFramework_batch.py" + " " + x
+
+  print "command  = ", command
+ 
+  #When using SendCluster_Push using PYTHON as a first argument, only command is taking into account (the argument option is trashed)
+  #LaunchOnCondor.SendCluster_Push(["PYTHON", command, option ])
+  LaunchOnCondor.SendCluster_Push(["PYTHON", command])
+
+LaunchOnCondor.SendCluster_Submit()
+
+
