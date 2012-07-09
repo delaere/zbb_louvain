159808fcbfb034808379ac4a8c6c0a77c0de9ffa
Adrien Caudron <adrien.caudron@cern.ch>
2012-07-09 20:51:56 +0000

    updated of number of events

--- python/zbbCommons.py
+++ python/zbbCommons.py
@@ -42,9 +42,10 @@ class zbbnorm:
   #https://twiki.cern.ch/twiki/bin/viewauth/CMS/StandardModelCrossSections
   xsec_DYjets_7TeV=3048.  #Ml+l->50, NNLO for Z->ll
   xsec_TTjets_7TeV=157.5  #NLO inclusive
-  xsec_TTpow_7TeV=16.7    #ttbar->llvvbb_
+  xsec_TTlept_7TeV=16.7   #ttbar->llvvbb_
   xsec_ZZ_7TeV=6.206      #cms measurement EWK-11-010 (2011)
-  xsec_ZH_125_7TeV=0.0189 #ZHxsec"0.3158"*BR(H->bb)"0.577"*BR(Z->ll)"0.10399" , here l=e, mu or tau : https://twiki.cern.ch/twiki/bin/view/LHCPhysics/CrossSections#Higgs_cross_sections_at_7_8_and
+  xsec_ZH125_7TeV=0.0189  #ZHxsec"0.3158"*BR(H->bb)"0.577"*BR(Z->ll)"0.10399" , here l=e, mu or tau : https://twiki.cern.ch/twiki/bin/view/LHCPhysics/CrossSections#Higgs_cross_sections_at_7_8_and
+  #xsec_ZH 115, 120, 130 and 135 to be added
   xsec_tW_7TeV=5.3        #NLO inclusive
   xsec_tbarW_7TeV=5.3     #NLO inclusive
   
@@ -53,16 +54,20 @@ class zbbnorm:
   xsec_DYjets_8TeV=3503.71  #Ml+l->50, NNLO for Z->ll
   xsec_TTjets_8TeV=225.197  #NLO inclusive
   xsec_ZZ_8TeV= 8.25561     #NLO inclusive Ml+l->12
-  xsec_ZH_125_8TeV=0.0237   #ZHxsec"0.3943 "*BR(H->bb)"0.577"*BR(Z->ll)"0.10399" , here l=e, mu or tau
+  xsec_ZH125_8TeV=0.0237    #ZHxsec"0.3943 "*BR(H->bb)"0.577"*BR(Z->ll)"0.10399" , here l=e, mu or tau
   xsec_tW_8TeV=11.1         #approx. NNLO inclusive
   xsec_tbarW_8TeV=11.1      #approx. NNLO inclusive
 
   #number of events processed for the PATtuple production
-  nev_DY_fall11=35907791.       #1st prod. to be updated when new DY will be available
-  nev_TT_fall1=37710785         #to be updated with matching
-  nev_ZZ_fall11=4191045.        #if needed to be updated with matching
-  nev_ZH_125_fall11=1100000.    #if needed to be updated with matching
-  nev_TTpowheg_fall11=16352171. #if needed to be updated with matching
-  nev_tW_fall11=814390.         #if needed to be updated with matching
-  nev_tbarW_fall11=809984.      #if needed to be updated with matching
+  nev_DY_fall11       = 36264432 # updated
+  nev_TT_fall1        = 59244088 # updated
+  nev_ZZ_fall11       =  4191045 # updated
+  nev_ZH115_fall11    =  1090000 # updated
+  nev_ZH120_fall11    =  1090000 # updated
+  nev_ZH125_fall11    =  1100000 # updated
+  nev_ZH130_fall11    =  1100000 # updated
+  nev_ZH135_fall11    =  1096956 # updated
+  nev_TTpowheg_fall11 = 16352171 # if needed to be updated with matching
+  nev_tW_fall11       =   814390 # updated
+  nev_tbarW_fall11    =   809984 # updated
   
