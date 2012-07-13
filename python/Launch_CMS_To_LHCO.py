04b3d4c3385462f51d37bbf9a89b622479c122ab
Jesus Vizan <Jesus.Manuel.Vizan.Garcia@cern.ch>
2012-07-13 13:37:53 +0000

    Ignore previous comment. The previuos update changed the reference to the script to point to the relative path instead of a particular version of the script in /home/fynu/. In this update some clean-up is made

--- python/Launch_CMS_To_LHCO.py
+++ python/Launch_CMS_To_LHCO.py
@@ -15,51 +15,6 @@ narguments = len(sys.argv)
 if narguments != 4:
   print "Usage: ", sys.argv[0], " foldername njobs suffix"
   print "Examples"
-  print "python ", sys.argv[0], " /storage/data/cms/users/llbb/mc_DYJets/Summer11_DYJets/DYJets/ 82 _DYmumu"
-  print "python ", sys.argv[0], " /storage/data/cms/users/llbb/mc_DYJets/Summer11_DYJets/DYJets/ 82 _DYee"
-  print "python ", sys.argv[0], " /storage/data/cms/users/llbb/mc_TTJets/Summer11_TTJets/TTJets/ 31 _TTmumu"
-  print "python ", sys.argv[0], " /storage/data/cms/users/llbb/mc_TTJets/Summer11_TTJets/TTJets/ 31 _TTee"
-  print "python ", sys.argv[0], " /storage/data/cms/users/llbb/mc_ZZ/Summer11/ 11 _ZZmumu"
-  print "python ", sys.argv[0], " /storage/data/cms/users/llbb/mc_ZZ/Summer11/ 11 _ZZee"
-  print "python ", sys.argv[0], " /storage/data/cms/users/llbb/TTbar_pow/ 146 _TTpowee"
-  print "python ", sys.argv[0], " /storage/data/cms/users/llbb/TTbar_pow/ 146 _TTpowmumu"
-  print "python ", sys.argv[0], " /storage/data/cms/users/llbb/mc_Higgstobb/Summer11/ 37 _ZH125mumu"
-  print "python ", sys.argv[0], " /storage/data/cms/users/llbb/mc_Higgstobb/Summer11/ 37 _ZH125ee"
-
-
-  print "python ", sys.argv[0], " /home/fynu/vizangarciaj/scratch/DYJets_Summer11_fewfiles/ 7 _DY7files"
-  
-  print "python ", sys.argv[0], " /home/fynu/vizangarciaj/scratch/DATA_2fb_Ele/ 73 _DATAee"
-  print "python ", sys.argv[0], " /home/fynu/vizangarciaj/scratch/DATA_2fb_Muon/ 137 _DATAmumu"
-
-
-
-  print "python ", sys.argv[0], " /storage/data/cms/users/arnaudp/Zbb_Prod_local/FARM_Zllbb_PAT_step/outputs/ 341 _Zbb4flvee"
-  print "python ", sys.argv[0], " /storage/data/cms/users/arnaudp/Zbb_Prod_local/FARM_Zllbb_PAT_step/outputs/ 341 _Zbb4flvmumu"
-
-  print "python ", sys.argv[0], " /storage/data/cms/users/llbb/Prod_data_2012A/DoubleMuCert_190456-191276/ 150 _DATA12Amumu"
-
-  print "python ", sys.argv[0], " /storage/data/cms/users/rcastello/Ele_2011A_44X_newAl/ 119 _test44"
-
-  print "python ", sys.argv[0], " /storage/data/cms/users/llbb/production2012_44X/Fall11_TTbar/ 363 _TTtest44"
-
-  print "python ", sys.argv[0], " /storage/data/cms/users/llbb/production2012_44X/Fall11_ZZ/ 841 _1fileperjobZZTest44"
-  print "python ", sys.argv[0], " /storage/data/cms/users/llbb/production2012_44X/Fall11_ZZ/ 29 _ZZTest44"
-
-  print "python ", sys.argv[0], " /storage/data/cms/users/llbb/productionJune2012_444/Fall11_Hbb/ 55 _ZHbb125"
-  print "python ", sys.argv[0], " /storage/data/cms/users/llbb/productionJune2012_444/Fall11_T_tW/ 17 _tW"
-  print "python ", sys.argv[0], " /storage/data/cms/users/llbb/productionJune2012_444/Fall11_Tbar_tW/ 18 _tbarW"
-  print "python ", sys.argv[0], " /storage/data/cms/users/llbb/productionJune2012_444/Fall11_TT_powheg/ 47 _TTpow"
-
-  print "python ", sys.argv[0], " /storage/data/cms/users/llbb/productionJune2012_444/Fall11_ZZ/ 421 _ZZ"
-
-
-  print "python ", sys.argv[0], " /storage/data/cms/store/user/acaudron/Fall11MC_444/Fall11_DYjets_v4/acaudron/DYJetsToLL_TuneZ2_M-50_7TeV-madgraph-tauola/production5fb_June2012_Fall11_DYjets_v4/918089973f5ad18b0c79d580ed9142aa/ 1451 _DY"
-
-
-  print "python ", sys.argv[0], " /home/fynu/vizangarciaj/scratch/DY444secondProdMissingFile/ 1 _DY1missingfileEE"
-  print "python ", sys.argv[0], " /home/fynu/vizangarciaj/scratch/DY444secondProdMissingFile/ 1 _DY1missingfileMM"
-
   print "python ", sys.argv[0], " /storage/data/cms/users/llbb/productionJune2012_444/MCwithMatching/zbbProd/ 3 _Zbb4Flavee"
   print "python ", sys.argv[0], " /storage/data/cms/users/llbb/productionJune2012_444/MCwithMatching/zbbProd/ 3 _Zbb4Flavmumu"
 
