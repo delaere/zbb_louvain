69cdce1ed812ac5332546fa9ed87898f405fa79d
Christophe Delaere <christophe.delaere@cern.ch>
2012-08-20 20:19:43 +0000

    proper implementation of JES and JER uncertainties for 2011

--- python/JetCorrectionUncertainty.py
+++ python/JetCorrectionUncertainty.py
@@ -1,7 +1,7 @@
 import os
 import ROOT
 from math import sqrt
-from zbbCommons import zbbfile, zbblabel
+from zbbCommons import zbbfile,zbbsystematics
 ROOT.gSystem.Load("libFWCoreFWLite.so")
 ROOT.AutoLibraryLoader.enable()
 ROOT.gSystem.Load("libCondFormatsJetMETObjects.so")
@@ -14,7 +14,7 @@ class JetCorrectionUncertaintyProxy:
       print "Error: ",file, ": No such file."
       self._engine = None
     else:
-      self._engine = ROOT.JetCorrectionUncertainty(file)
+      self._engine = ROOT.JetCorrectionUncertainty(ROOT.JetCorrectorParameters(file, "Total"))
 
   def unc_tot_jet(self,jet):
     if self._engine is None: return 0
@@ -27,19 +27,23 @@ class JetCorrectionUncertaintyProxy:
     jetpt = jet.pt()
     # smear to reproduce resolution measured in data
     # apply JetMet recommendation:  pT->max[0.,pTgen+c*(pT-pTgen)] 
-    ptgen = jet.genJet().pt()
-    jersf = (jerCorrectionFactor(jet)-1.) * zbblabel.JERfactor
+    try:
+      ptgen = jet.genJet().pt()
+    except:
+      ptgen = jetpt # here, we could do something more clever. The official recipe involves randomly sampling the measured resolution.
+    jersf = (self.jerCorrectionFactor(jet)-1.) * zbbsystematics.JERfactor
     jetpt = max(0., ptgen + (1.+jersf)*(jetpt-ptgen))
     # take into account JEC uncertainty
-    jesunc = unc_tot_jet(jet) * zbblabel.JESfactor
-    jetpt = jetpt + jesunc
+    jesunc = self.unc_tot_jet(jet) * zbbsystematics.JESfactor * jetpt
+    jetpt = max(0., jetpt + jesunc)
     # return the jet pt, including all of the above
     return jetpt
 
-  def jerCorrectionFactor(self,jet)
+  def jerCorrectionFactor(self,jet):
     eta = abs(jet.eta())
-    if eta<0.5: return 1.052
+    if   eta<0.5: return 1.052
     elif eta<1.1: return 1.057
     elif eta<1.7: return 1.096
     elif eta<2.3: return 1.134
-    else return 1.288 # valid up to 5.0
+    else :        return 1.288 # valid up to 5.0
+
