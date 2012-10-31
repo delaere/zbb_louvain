a77fa8fa38d52f6e30ea7a7998c7594108104031
Arnaud Pin <arnaud.pin@cern.ch>
2012-10-31 14:46:47 +0000

    MLP_Higgs_vs_bkg_MU.h

--- scripts/ME_Analysis/Merging/MLP_Higgs_vs_bkg_MU.cxx
+++ scripts/ME_Analysis/Merging/MLP_Higgs_vs_bkg_MU.cxx
@@ -7,7 +7,7 @@ double MLP_Higgs_vs_bkg_MU::Value(int index,double in0,double in1,double in2) {
    input2 = (in2 - 1.27601)/0.269311;
    switch(index) {
      case 0:
-         return neuron0xc147670();
+         return neuron0xbe46320();
      default:
          return 0.;
    }
@@ -19,201 +19,201 @@ double MLP_Higgs_vs_bkg_MU::Value(int index, double* input) {
    input2 = (input[2] - 1.27601)/0.269311;
    switch(index) {
      case 0:
-         return neuron0xc147670();
+         return neuron0xbe46320();
      default:
          return 0.;
    }
 }
 
-double MLP_Higgs_vs_bkg_MU::neuron0xc134810() {
+double MLP_Higgs_vs_bkg_MU::neuron0xbe334c0() {
    return input0;
 }
 
-double MLP_Higgs_vs_bkg_MU::neuron0xc134b50() {
+double MLP_Higgs_vs_bkg_MU::neuron0xbe33800() {
    return input1;
 }
 
-double MLP_Higgs_vs_bkg_MU::neuron0xc145b20() {
+double MLP_Higgs_vs_bkg_MU::neuron0xbe447d0() {
    return input2;
 }
 
-double MLP_Higgs_vs_bkg_MU::input0xc145e70() {
-   double input = 1.31904;
-   input += synapse0xc0fd060();
-   input += synapse0xc11b5b0();
-   input += synapse0xc146120();
+double MLP_Higgs_vs_bkg_MU::input0xbe44b20() {
+   double input = -0.850348;
+   input += synapse0xbdfbd10();
+   input += synapse0xbe1a260();
+   input += synapse0xbe44dd0();
    return input;
 }
 
-double MLP_Higgs_vs_bkg_MU::neuron0xc145e70() {
-   double input = input0xc145e70();
+double MLP_Higgs_vs_bkg_MU::neuron0xbe44b20() {
+   double input = input0xbe44b20();
    return ((input < -709. ? 0. : (1/(1+exp(-input)))) * 1)+0;
 }
 
-double MLP_Higgs_vs_bkg_MU::input0xc146160() {
-   double input = 0.881956;
-   input += synapse0xc1464a0();
-   input += synapse0xc1464e0();
-   input += synapse0xc146520();
+double MLP_Higgs_vs_bkg_MU::input0xbe44e10() {
+   double input = 1.22273;
+   input += synapse0xbe45150();
+   input += synapse0xbe45190();
+   input += synapse0xbe451d0();
    return input;
 }
 
-double MLP_Higgs_vs_bkg_MU::neuron0xc146160() {
-   double input = input0xc146160();
+double MLP_Higgs_vs_bkg_MU::neuron0xbe44e10() {
+   double input = input0xbe44e10();
    return ((input < -709. ? 0. : (1/(1+exp(-input)))) * 1)+0;
 }
 
-double MLP_Higgs_vs_bkg_MU::input0xc146560() {
-   double input = 0.492398;
-   input += synapse0xc1468a0();
-   input += synapse0xc1468e0();
-   input += synapse0xc146920();
+double MLP_Higgs_vs_bkg_MU::input0xbe45210() {
+   double input = 0.847483;
+   input += synapse0xbe45550();
+   input += synapse0xbe45590();
+   input += synapse0xbe455d0();
    return input;
 }
 
-double MLP_Higgs_vs_bkg_MU::neuron0xc146560() {
-   double input = input0xc146560();
+double MLP_Higgs_vs_bkg_MU::neuron0xbe45210() {
+   double input = input0xbe45210();
    return ((input < -709. ? 0. : (1/(1+exp(-input)))) * 1)+0;
 }
 
-double MLP_Higgs_vs_bkg_MU::input0xc146960() {
-   double input = 1.68812;
-   input += synapse0xc146ca0();
-   input += synapse0xc146ce0();
-   input += synapse0xc146d20();
+double MLP_Higgs_vs_bkg_MU::input0xbe45610() {
+   double input = 0.18467;
+   input += synapse0xbe45950();
+   input += synapse0xbe45990();
+   input += synapse0xbe459d0();
    return input;
 }
 
-double MLP_Higgs_vs_bkg_MU::neuron0xc146960() {
-   double input = input0xc146960();
+double MLP_Higgs_vs_bkg_MU::neuron0xbe45610() {
+   double input = input0xbe45610();
    return ((input < -709. ? 0. : (1/(1+exp(-input)))) * 1)+0;
 }
 
-double MLP_Higgs_vs_bkg_MU::input0xc146d60() {
-   double input = -0.629308;
-   input += synapse0xc1470a0();
-   input += synapse0xc1470e0();
-   input += synapse0xc147120();
-   input += synapse0xc147160();
+double MLP_Higgs_vs_bkg_MU::input0xbe45a10() {
+   double input = -0.333344;
+   input += synapse0xbe45d50();
+   input += synapse0xbe45d90();
+   input += synapse0xbe45dd0();
+   input += synapse0xbe45e10();
    return input;
 }
 
-double MLP_Higgs_vs_bkg_MU::neuron0xc146d60() {
-   double input = input0xc146d60();
+double MLP_Higgs_vs_bkg_MU::neuron0xbe45a10() {
+   double input = input0xbe45a10();
    return ((input < -709. ? 0. : (1/(1+exp(-input)))) * 1)+0;
 }
 
-double MLP_Higgs_vs_bkg_MU::input0xc1471a0() {
-   double input = -0.365523;
-   input += synapse0xc1474e0();
-   input += synapse0xbf22140();
-   input += synapse0xbf22180();
-   input += synapse0xc147630();
+double MLP_Higgs_vs_bkg_MU::input0xbe45e50() {
+   double input = -1.29751;
+   input += synapse0xbe46190();
+   input += synapse0xbc20df0();
+   input += synapse0xbc20e30();
+   input += synapse0xbe462e0();
    return input;
 }
 
-double MLP_Higgs_vs_bkg_MU::neuron0xc1471a0() {
-   double input = input0xc1471a0();
+double MLP_Higgs_vs_bkg_MU::neuron0xbe45e50() {
+   double input = input0xbe45e50();
    return ((input < -709. ? 0. : (1/(1+exp(-input)))) * 1)+0;
 }
 
-double MLP_Higgs_vs_bkg_MU::input0xc147670() {
-   double input = -0.226891;
-   input += synapse0xc147890();
-   input += synapse0xc1478d0();
+double MLP_Higgs_vs_bkg_MU::input0xbe46320() {
+   double input = -0.370862;
+   input += synapse0xbe46540();
+   input += synapse0xbe46580();
    return input;
 }
 
-double MLP_Higgs_vs_bkg_MU::neuron0xc147670() {
-   double input = input0xc147670();
+double MLP_Higgs_vs_bkg_MU::neuron0xbe46320() {
+   double input = input0xbe46320();
    return (input * 1)+0;
 }
 
-double MLP_Higgs_vs_bkg_MU::synapse0xc0fd060() {
-   return (neuron0xc134810()*-1.89004);
+double MLP_Higgs_vs_bkg_MU::synapse0xbdfbd10() {
+   return (neuron0xbe334c0()*0.841371);
 }
 
-double MLP_Higgs_vs_bkg_MU::synapse0xc11b5b0() {
-   return (neuron0xc134b50()*0.486873);
+double MLP_Higgs_vs_bkg_MU::synapse0xbe1a260() {
+   return (neuron0xbe33800()*-0.0478218);
 }
 
-double MLP_Higgs_vs_bkg_MU::synapse0xc146120() {
-   return (neuron0xc145b20()*-0.0267085);
+double MLP_Higgs_vs_bkg_MU::synapse0xbe44dd0() {
+   return (neuron0xbe447d0()*-0.899818);
 }
 
-double MLP_Higgs_vs_bkg_MU::synapse0xc1464a0() {
-   return (neuron0xc134810()*-0.467487);
+double MLP_Higgs_vs_bkg_MU::synapse0xbe45150() {
+   return (neuron0xbe334c0()*0.96203);
 }
 
-double MLP_Higgs_vs_bkg_MU::synapse0xc1464e0() {
-   return (neuron0xc134b50()*-0.0612667);
+double MLP_Higgs_vs_bkg_MU::synapse0xbe45190() {
+   return (neuron0xbe33800()*0.278344);
 }
 
-double MLP_Higgs_vs_bkg_MU::synapse0xc146520() {
-   return (neuron0xc145b20()*0.514962);
+double MLP_Higgs_vs_bkg_MU::synapse0xbe451d0() {
+   return (neuron0xbe447d0()*0.469051);
 }
 
-double MLP_Higgs_vs_bkg_MU::synapse0xc1468a0() {
-   return (neuron0xc134810()*-0.321481);
+double MLP_Higgs_vs_bkg_MU::synapse0xbe45550() {
+   return (neuron0xbe334c0()*-1.59739);
 }
 
-double MLP_Higgs_vs_bkg_MU::synapse0xc1468e0() {
-   return (neuron0xc134b50()*0.074264);
+double MLP_Higgs_vs_bkg_MU::synapse0xbe45590() {
+   return (neuron0xbe33800()*0.515265);
 }
 
-double MLP_Higgs_vs_bkg_MU::synapse0xc146920() {
-   return (neuron0xc145b20()*-0.0842636);
+double MLP_Higgs_vs_bkg_MU::synapse0xbe455d0() {
+   return (neuron0xbe447d0()*-0.345508);
 }
 
-double MLP_Higgs_vs_bkg_MU::synapse0xc146ca0() {
-   return (neuron0xc134810()*0.491302);
+double MLP_Higgs_vs_bkg_MU::synapse0xbe45950() {
+   return (neuron0xbe334c0()*-0.229593);
 }
 
-double MLP_Higgs_vs_bkg_MU::synapse0xc146ce0() {
-   return (neuron0xc134b50()*0.463572);
+double MLP_Higgs_vs_bkg_MU::synapse0xbe45990() {
+   return (neuron0xbe33800()*-0.131975);
 }
 
-double MLP_Higgs_vs_bkg_MU::synapse0xc146d20() {
-   return (neuron0xc145b20()*0.580597);
+double MLP_Higgs_vs_bkg_MU::synapse0xbe459d0() {
+   return (neuron0xbe447d0()*0.0893068);
 }
 
-double MLP_Higgs_vs_bkg_MU::synapse0xc1470a0() {
-   return (neuron0xc145e70()*-1.26767);
+double MLP_Higgs_vs_bkg_MU::synapse0xbe45d50() {
+   return (neuron0xbe44b20()*-0.455112);
 }
 
-double MLP_Higgs_vs_bkg_MU::synapse0xc1470e0() {
-   return (neuron0xc146160()*-1.50654);
+double MLP_Higgs_vs_bkg_MU::synapse0xbe45d90() {
+   return (neuron0xbe44e10()*0.457998);
 }
 
-double MLP_Higgs_vs_bkg_MU::synapse0xc147120() {
-   return (neuron0xc146560()*-0.366042);
+double MLP_Higgs_vs_bkg_MU::synapse0xbe45dd0() {
+   return (neuron0xbe45210()*0.393809);
 }
 
-double MLP_Higgs_vs_bkg_MU::synapse0xc147160() {
-   return (neuron0xc146960()*1.04086);
+double MLP_Higgs_vs_bkg_MU::synapse0xbe45e10() {
+   return (neuron0xbe45610()*-0.0714145);
 }
 
-double MLP_Higgs_vs_bkg_MU::synapse0xc1474e0() {
-   return (neuron0xc145e70()*-1.30754);
+double MLP_Higgs_vs_bkg_MU::synapse0xbe46190() {
+   return (neuron0xbe44b20()*1.03326);
 }
 
-double MLP_Higgs_vs_bkg_MU::synapse0xbf22140() {
-   return (neuron0xc146160()*-0.215257);
+double MLP_Higgs_vs_bkg_MU::synapse0xbc20df0() {
+   return (neuron0xbe44e10()*0.940893);
 }
 
-double MLP_Higgs_vs_bkg_MU::synapse0xbf22180() {
-   return (neuron0xc146560()*-1.17432);
+double MLP_Higgs_vs_bkg_MU::synapse0xbc20e30() {
+   return (neuron0xbe45210()*-1.78763);
 }
 
-double MLP_Higgs_vs_bkg_MU::synapse0xc147630() {
-   return (neuron0xc146960()*0.875143);
+double MLP_Higgs_vs_bkg_MU::synapse0xbe462e0() {
+   return (neuron0xbe45610()*-1.09366);
 }
 
-double MLP_Higgs_vs_bkg_MU::synapse0xc147890() {
-   return (neuron0xc146d60()*1.8369);
+double MLP_Higgs_vs_bkg_MU::synapse0xbe46540() {
+   return (neuron0xbe45a10()*0.543581);
 }
 
-double MLP_Higgs_vs_bkg_MU::synapse0xc1478d0() {
-   return (neuron0xc1471a0()*1.1924);
+double MLP_Higgs_vs_bkg_MU::synapse0xbe46580() {
+   return (neuron0xbe45e50()*2.52276);
 }
 
