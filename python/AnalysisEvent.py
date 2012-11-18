b2e9761a9fcfb3d531085a1990cca752b50edf1c
Christophe Delaere <christophe.delaere@cern.ch>
2012-11-18 16:46:58 +0000

    /!\ still in development. Functionalities in place, but needs test/debug/validation.

---
+++ python/AnalysisEvent.py
@@ -0,0 +1,294 @@
+from DataFormats.FWLite import Events, Handle
+from FWCore.ParameterSet.Types import InputTag
+import inspect
+
+class AnalysisEvent(Events):
+   """A class that complements fwlite::Events with analysis facilities.
+      The class provides the following additional functionalities:
+        1. instrumentation for event weight
+             A set of weight classes can be defined, and the event weight
+             is computed and cached using those.
+        2. list of event products used in the analysis
+             It allows to ask later on simply for "jets" or "electrons"
+             to run the fwlite::Handle machinery and/or get the cached result.
+        3. a list of "producers" of analysis high-level quantities
+             It allows to run "analysis on demand", by automatically running
+             the defined producers to fill the cache, and later use that one.
+        4. a volatile dictionary
+             It allows to use the event as an heterogenous container for
+             any analysis product. The event is properly reset when iterating 
+             to the next event.
+   """
+
+   def __init__(self, inputFiles = '', **kwargs):
+     """Initialize the AnalysisEvent like a standard Event, plus additional features."""
+     # initialization of base functionalities
+     Events.__init__(inputFiles,kwargs)
+     # additional features:
+     # 1. instrumentation for event weight
+     self._weightCache = {}
+     self._weightEngines = {}
+     # 2. a list of event products used in the analysis
+     self._collections = {} 
+     # 3. a list of "producers" of analysis high-level quantities
+     self._producers = {}
+     # 4. volatile dictionary. User can add any quantity to the event and it will be 
+     #    properly erased in the iteration step. 
+     self.__dict__["vardict"] = {}
+     
+   def addWeight(self, name, weightClass):
+     """Declare a new class (engine) to compute the weights.
+        weightClass must have a weight() method returning a float."""
+     if name in self._weightEngines:
+       raise KeyError("%r weight engine is already declared", name)
+     self._weightEngines[name] = weightClass
+
+   def delWeight(self, name):
+     """Remove one weight engine from the internal list."""
+     # just to clean the dictionnary
+     del self._weightEngines[name]
+
+   def weight(self, weightList, **kwargs):
+     """Return the event weight. Arguments:
+         * weightList is the list of engines to use, as a list of strings.
+         * the other named arguments are forwarded to the engines.
+        The output is the product of the selected individual weights."""
+     # first check in the cache if the result is there already
+     kwargs["weightList"] = weightList
+     # compute the weight or use the cached value
+     if not kwargs in self._weightCache : 
+       w = 1.
+       for weightElement in weightList:
+         engine = self._weightEngines[weightElement]
+         engineArgs = inspect.getargspec(engine.weight).args
+         subargs = dict((k,v) for k,v in kwargs.iteritems if k in engineArgs)
+         subargs["weightList"] = weightElement
+         w *= self._weightCache.setdefault(subargs,engine.weight(subargs))
+       self._weightCache[kwargs] = w
+     return self._weightCache[kwargs]
+
+   def addCollection(self, name, handle, inputTag):
+     """Register an event collection as used by the analysis.
+        Example: addCollection("jets","vector<pat::Jet>","cleanPatJets" """
+     if name in self._collections:
+       raise KeyError("%r collection is already declared", name)
+     if name in self._producers:
+       raise KeyError("%r is already declared as a producer", name)
+     if hasattr(self,name):
+       raise AttributeError("%r object already has attribute %r" % (type(self).__name__, attr))
+     self._collections[name] = {"handle":Handle(handle),"collection":inputTag}
+
+   def removeCollection(self,name):
+     """Forget about the named event collection.
+        This method will delete both the product from the cache (if any) and the definition.
+        To simply clear the cache, use "del event.name" instead. """
+     del self._collections[name]
+     if name in self.vardict:
+       delattr(self,name)
+
+   def getCollection(self,name):
+     """Retrieve the event product or return the cached collection.
+        Note that the prefered way to get the collection is instead to access the "event.name" attribute."""
+     if not name in self._collections:
+       raise AttributeError("%r object has no attribute %r" % name)
+     if not name in self.vardict:
+       self.getByLabel(self._collections[name]["collection"],self._collections[name]["handle"])
+       setattr(self,name,self._collections[name]["handle"].product())
+     return getattr(self,name)
+
+   def addProducer(self,name,producer):
+     """Register a producer to create new high-level analysis objects."""
+     if name in self._producers:
+       raise KeyError("%r producer is already declared", name)
+     if name in self._collections:
+       raise KeyError("%r is already declared as a collection", name)
+     if hasattr(self,name):
+       raise AttributeError("%r object already has attribute %r" % (type(self).__name__, attr))
+     self._producers[name] = producer
+
+   def removeProducer(self,name):
+     """Forget about the producer.
+        This method will delete both the product from the cache (if any) and the producer.
+        To simply clear the cache, use "del event.name" instead."""
+     del self._producers[name]
+     if name in self.vardict:
+       delattr(self,name)
+
+   def _next(self):
+     """(Internal) Iterator internals"""
+     # standard part of the iteration
+     Events._next()
+     # additional features
+     self.vardict.clear()
+     self._weightCache.clear()
+
+   def __getattr__(self, attr):
+     """Overloaded getter to handle properly:
+          - volatile analysis objects
+          - event collections
+          - data producers"""
+     print self.__dict__
+     if attr in self.__dict__["vardict"]:
+       return self.vardict[attr]
+     if attr in self._collections:
+       return getCollection(attr)
+     if attr in self._producers:
+       return self.vardict.setdefault(attr, self._producers[attr](self))
+     raise AttributeError("%r object has no attribute %r" % (type(self).__name__, attr))
+
+   def __setattr__(self, name, value):
+     """Overloaded setter that puts any new attribute in the volatile dict."""
+     if name in self.__dict__  or not "vardict" in self.__dict__:
+       self.__dict__[name] = value
+     else:
+       if name in self._collections or name in self._producers:
+         raise AttributeError("%r object %r attribute is read-only (event collection)" % (type(self).__name__, name))
+       self.vardict[name] = value
+
+   def __delattr__(self, name):
+     """Overloaded del method to handle the volatile internal dictionary."""
+     if name=="vardict":
+       raise AttributeError("%r object has no attribute %r" % (type(self).__name__, name))
+     if name in self.__dict__:
+       del self.__dict__[name]
+     elif name in self.vardict:
+       del self.vardict[name]
+     else:
+       raise AttributeError("%r object has no attribute %r" % (type(self).__name__, name))
+
+   def __str__(self):
+     """Event text dump."""
+     dictjoin = lambda d,j='\t=> ',s='\n': s.join([j.join((str(k),str(v))) for k,v in d.iteritems()])
+     mystring = "=================================================================\n"
+     # general information
+     mystring += "Run %d - Lumisection %d, Event %d\n" % (self.eventAuxiliary().run(), 
+                                                          self.eventAuxiliary().luminosityBlock(),
+                                                          self.eventAuxiliary().id().event())
+     mystring += "Recorded on %s\n" % datetime.fromtimestamp(self.eventAuxiliary().time().unixTime()).strftime("%Y-%m-%d %H:%M:%S")
+     mystring += "-----------------------------------------------------------------\n"
+     # weights
+     if len(self._weightCache)==0: 
+       mystring += "No weight computed so far. Default weight is %f.\n" % self.weight(weightList=self._weightEngines.keys())
+       #TODO: if that "default weight" works, then implement the "default weight" in the weight method.
+     else:
+       mystring += "Weights:\n"
+       mystring += dictjoin(self._weightCache)
+     mystring += "-----------------------------------------------------------------\n"
+     # list the collections
+     for colname in self._collections.keys():
+       collection = self.getCollection(colname)
+       handle = self._collections[name]["handle"]
+       try:
+         mystring += "*** %s has %d elements\n" % (colname,len(collection))
+       except TypeError:
+         pass
+       if "reco::Vertex" in colname:
+         mystring += reduce(lambda a,b: a+b,map(self._vertexString,collection))
+       elif "pat::Electron" in colname:
+         mystring += reduce(lambda a,b: a+b,map(self._electronString,collection))
+       elif "pat::Muon" in colname:
+         mystring += reduce(lambda a,b: a+b,map(self._muonString,collection))
+       elif "pat::Jet" in colname:
+         mystring += reduce(lambda a,b: a+b,map(self._jetString,collection))
+       elif "pat::MET" in colname:
+         mystring += reduce(lambda a,b: a+b,map(self._metString,collection))
+       elif "reco::CompositeCandidate" in colname:
+         mystring += reduce(lambda a,b: a+b,map(self._candidateString,collection))
+     mystring += "-----------------------------------------------------------------\n"
+     # list the registered producers
+     mystring += "Producers:\n"
+     mystring += dictjoin(self._producers)
+     mystring += "-----------------------------------------------------------------\n"
+     # list the content of vardict, excluding producers and collections
+     mystring += "Content of the cache:\n"
+     mystring += dictjoin(dict((k,v) for k, v in self.vardict.iteritems() if not (k in self._producers.keys() or k in self._collections.keys())))
+     return mystring
+
+   def _vertexString(self, vertex):
+     theString =  "Vertex position: (%f,%f,%f) +/- (%f,%f,%f)\n" % (vertex.x(), vertex.y(),vertex.z(),vertex.xError(),vertex.yError(),vertex.zError())
+     theString += "  Number of tracks: %d\n" % vertex.tracksSize()
+     theString += "  chi2/ndof: %f/%d\n" % (vertex.chi2(),vertex.ndof())
+     return theString
+
+   def _lorentzVectorString(self, label, lorentzVector):
+     theString =  "%s candidate\n" % label
+     theString += "   (pt, eta, phi) = (%f,%f,%f)\n" % (lorentzVector.Pt(), lorentzVector.Eta(), lorentzVector.Phi())
+     theString += "   mass = %f GeV\n" % lorentzVector.M() 
+     theString += "   p = %f GeV; mt = %f GeV\n" % (lorentzVector.P(), lorentzVector.Mt())
+     return theString
+
+   def _candidateString(self, label, candidate):
+     theString =  "%s candidate\n" % label
+     theString += "  (pt, eta, phi) = (%f,%f,%f)\n" % (candidate.pt(), candidate.eta(), candidate.phi())
+     theString += "  mass = %f GeV\n" % candidate.mass()
+     theString += "  charge: %d\n" % candidate.charge()
+     theString += "  p = %f GeV; mt = %f GeV\n" % (candidate.p(), candidate.mt())
+     return theString
+
+   def _metString(self, met):
+     theString = "Missing Et: %f at phi= %f with a significance of %f\n" % (met.et(),met.phi(),met.significance())
+     return theString
+
+   def _jetString(self, jet):
+     theString =  self._candidateString("jet",jet)
+     theString += "  nhf=%f\n" % (( jet.neutralHadronEnergy() + jet.HFHadronEnergy() ) / jet.energy())
+     theString += "  nef=%f\n" % (jet.neutralEmEnergyFraction())
+     theString += "  nconstituents=%d\n" % (jet.numberOfDaughters())
+     theString += "  chf=%f\n" % (jet.chargedHadronEnergyFraction())
+     theString += "  nch=%d\n" % (jet.chargedMultiplicity())
+     theString += "  cef=%f\n" % (jet.chargedEmEnergyFraction())
+     theString += "  B-tagging information:\n"
+     theString += "     SSVHE: %f\n" % jet.bDiscriminator("simpleSecondaryVertexHighEffBJetTags")
+     theString += "     SSVHP: %f\n" % jet.bDiscriminator("simpleSecondaryVertexHighPurBJetTags")
+     taginfo = jet.tagInfoSecondaryVertex("secondaryVertex")
+     if not taginfo is None and jet.bDiscriminator("simpleSecondaryVertexHighEffBJetTags")>0:
+       sv = taginfo.secondaryVertex(0)
+       if not sv is None:
+         distance = taginfo.flightDistance(0,True)
+         dir = taginfo.flightDirection(0)
+         dirv = ROOT.TVector3(dir.x(),dir.y(),dir.z())
+         dirj = ROOT.TVector3(jet.px(),jet.py(),jet.pz())
+         theString += "     details about the secondary vertex:\n"
+         theString += "     * number of tracks: %d\n" % sv.tracksSize()
+         theString += "     * chi2: %f\n" % sv.chi2()
+         theString += "     * distance: %f+/-%f" % (distance.value(),distance.error())
+         theString += "     * distance significance: %f\n" % distance.significance()
+         theString += "     * flight direction: %f,%f,%f\n" % (dir.x(),dir.y(),dir.z())
+         theString += "     * dR(jet): %f\n" % dirv.DeltaR(dirj)
+         theString += "     * mass: %f\n" % sv.p4().M()
+     return theString
+
+   def _electronString(self, electron):
+     theString =  self._candidateString("electron",electron)
+     scEt = (electron.ecalEnergy()*sin(electron.theta()))
+     superclusterEta = abs(electron.superCluster().eta())
+     theString += "  Number of missing hits: %d\n" % electron.gsfTrack().numberOfLostHits()
+     theString += "  SuperCluster Et: %f\n" % scEt
+     theString += "  HCAL isolation: %f\n"  % electron.dr03HcalTowerSumEt()/scEt
+     theString += "  ECAL isolation: %f\n"  % electron.dr03EcalRecHitSumEt()/scEt
+     theString += "  Tk   isolation: %f\n"  % electron.dr03TkSumPt()/scEt
+     theString += "  H over E: %f\n" % electron.hadronicOverEm()
+     theString += "  dphi: %f\n" % electron.deltaPhiEleClusterTrackAtCalo()
+     theString += "  deta: %f\n" % electron.deltaEtaEleClusterTrackAtCalo()
+     theString += "  inin: %f\n" % electron.scSigmaIEtaIEta()
+     theString += "  d0: %f\n"   % abs(electron.dB())
+     theString += "  supercluster eta: %f\n" % electron.superCluster().eta(),
+     if superclusterEta<1.4442 or (superclusterEta>1.566 and superclusterEta<2.5 ):
+       print " => in fiducial region\n"
+     else:
+       print " => out of fiducial region\n"
+     return theString
+
+   def _muonString(self, muon):
+     theString =  self._candidateString("muon",muon)
+     if muon.isTrackerMuon():
+       print "  Number of hits (all, pixels, strips): %d,%d,%d\n" % (muon.innerTrack().numberOfValidHits(),
+                                                                     muon.innerTrack().hitPattern().numberOfValidPixelHits(),
+                                                                     muon.innerTrack().hitPattern().numberOfValidStripHits())
+     if muon.isGlobalMuon():
+       print "  Number of muon hits: %d\n" % muon.globalTrack().hitPattern().numberOfValidMuonHits()
+     if muon.isTrackerMuon() and muon.isGlobalMuon():
+       print "  Chi2: %f\n" % muon.normChi2()
+     print "  Isolation: (%f+%f)/pt = %f\n" % (muon.trackIso(),muon.caloIso(),(muon.trackIso()+muon.caloIso())/muon.pt())
+     return theString
+
