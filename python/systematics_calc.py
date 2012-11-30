ce2d78279ec2b89982423326a650a944578c76c7
Ludivine Ceard <Ludivine.Ceard@cern.ch>
2012-11-30 17:23:01 +0000

    bug in final result

--- python/systematics_calc.py
+++ python/systematics_calc.py
@@ -9,12 +9,8 @@ import math
 import datetime
 
 def syst(flavor,mode):
-    if mode=="JetEn_withoutJES":
-        mat_Down =[[3.17041303,-8.34993026],[-0.05246257, 7.15368494]]
-        ##print mat_Down[0][1],", ",mat_Down[1][0],", ",mat_Down[1][0],", ",mat_Down[1][1] 
-        mat_Up =[[3.17041303,-8.38016526],[-0.05246257, 7.17958835]]
 
-    elif mode=="MuonEn":
+    if mode=="MuonEn":
         mat_Down =[[3.17041294,-8.3521289],[-0.05246257,7.15556872]]
         mat_Up =[[3.17041303,-8.35431508],[-0.05246257,7.15744157]]
     
@@ -26,18 +22,33 @@ def syst(flavor,mode):
         mat_Down =[[4.18850418,-11.06452208],[ -0.06308005,9.33711608]]
         mat_Up =[[4.18850456,-11.06543221],[ -0.06308006,9.33788355]]
 
-        
-    elif mode=="JetEn":
-        mat_Down =[[3.23456058,-8.50565323],[-0.04498772,7.24478139]]
-        mat_Up =[[3.11955101,-8.44504028],[-0.06168477,7.25312268]]
+    elif mode=="JES_onjets": ##JES on jets only
+        mat_Down =[[3.35777268,-8.79206442],[-0.04706438,7.50540379]]
+        ##mat_Down =[[3.358,-8.792],[-0.047,7.505]]
+        mat_Up =[[3.25116496,-8.69646903],[-0.06481426,7.47060989]]
+        ##mat_Up =[[3.251,-8.696],[-0.065,7.471]]
 
-    elif mode=="JetRes":
+    elif mode=="JetEn_withoutJES": ##JES on MET only
+        mat_Down =[[3.17041303,-8.34993026],[-0.05246257, 7.15368494]]
+        ##print mat_Down[0][1],", ",mat_Down[1][0],", ",mat_Down[1][0],", ",mat_Down[1][1] 
+        mat_Up =[[3.17041303,-8.38016526],[-0.05246257, 7.17958835]]
+       
+    elif mode=="JetEn":  ## JES on jets and MET
+        mat_Down =[[3.35944331,-8.83778801],[-0.04672464,7.5342471]]
+        mat_Up =[[3.25221282,-8.80920526],[-0.0641893,7.57279609]]
+
+    elif mode=="JER": ##JER on jets only
+        mat_Down =[[3.26607861,-8.44377494],[-0.0495677,7.29783867]]
+        mat_Up =[[3.32386479,-9.0501284],[-0.06012931,7.66303096]]
+
+    elif mode=="JetRes_withoutJER":##JER on MET only 
+        mat_Down =[[3.1704131,-8.38183893],[-0.05246257,7.18102213]]
+        mat_Up =[[ 3.17041303,-8.33808209],[-0.05246257,7.14353419]]
+
+    elif mode=="JetRes": ##JER on jets and MET
         mat_Down =[[3.14583405,-8.17097585],[-0.0477428,7.05348283]]
         mat_Up =[[ 3.19705532,-8.70205262],[-0.05783531,7.36404113]]
 
-    elif mode=="JetRes_withoutJER":
-        mat_Down =[[3.1704131,-8.38183893],[-0.05246257,7.18102213]]
-        mat_Up =[[ 3.17041303,-8.33808209],[-0.05246257,7.14353419]]
 
     else :
         print " Mode not found"
@@ -70,7 +81,7 @@ def syst(flavor,mode):
     print "[ ", modified_yields_Up[0], " ]"
     print "[ ", modified_yields_Up[1], " ]"
 
-    print "sig Z+1b= ", abs(modified_yields_Down[0]-modified_yields_Up[0])*2/((modified_yields_Down[0]+modified_yields_Up[0])*lumi)*100, "%"
-    print "sig Z+2b= ", abs(modified_yields_Down[1]-modified_yields_Up[1])*2/((modified_yields_Down[1]+modified_yields_Up[1])*lumi)*100, "%"
+    print "sig Z+1b= ", abs(modified_yields_Down[0]-modified_yields_Up[0])*2/((modified_yields_Down[0]+modified_yields_Up[0]))*100, "%"
+    print "sig Z+2b= ", abs(modified_yields_Down[1]-modified_yields_Up[1])*2/((modified_yields_Down[1]+modified_yields_Up[1]))*100, "%"
 
     
