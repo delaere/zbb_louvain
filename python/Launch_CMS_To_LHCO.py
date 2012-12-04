98be912fb89cea311173343b77b1cb351e9b69f6
Jesus Vizan <Jesus.Manuel.Vizan.Garcia@cern.ch>
2012-12-04 10:15:40 +0000

    change the meaning of the numerical argument. Now it representes the number of rootfiles per section in the condor job

--- python/Launch_CMS_To_LHCO.py
+++ python/Launch_CMS_To_LHCO.py
@@ -28,10 +28,31 @@ if narguments != 4:
   print "python ", sys.argv[0], " /home/fynu/vizangarciaj/scratch/DATA_5fb_EleB/ 353 _DataEleB"
 
   print "python ", sys.argv[0], " /home/fynu/vizangarciaj/scratch/DATA_5fb_Mu/ 183 _DataMu"
+
+  print "python ", sys.argv[0], " /home/fynu/vizangarciaj/storage/DY444secondProdAllButMissingFile/ 1451 _DYmumu"
+  print "python ", sys.argv[0], " /home/fynu/vizangarciaj/storage/DY444secondProdAllButMissingFile/ 1451 _DYee"
+
+  print "python ", sys.argv[0], " /storage/data/cms/store/user/acaudron/Fall11MC_444/zbbProd/ 3 _Zbb4flveeNew"
+  print "python ", sys.argv[0], " /storage/data/cms/store/user/acaudron/Fall11MC_444/zbbProd/ 3 _Zbb4flvmumuNew"
+
+  print "python ", sys.argv[0], " /storage/data/cms/store/user/acaudron/Fall11MC_444/Fall11_DYjets_v4/acaudron/DYJetsToLL_TuneZ2_M-50_7TeV-madgraph-tauola/production5fb_June2012_Fall11_DYjets_v4/918089973f5ad18b0c79d580ed9142aa/ 66 _DYmumu"
+  print "python ", sys.argv[0], " /storage/data/cms/store/user/acaudron/Fall11MC_444/Fall11_DYjets_v4/acaudron/DYJetsToLL_TuneZ2_M-50_7TeV-madgraph-tauola/production5fb_June2012_Fall11_DYjets_v4/918089973f5ad18b0c79d580ed9142aa/ 66 _DYee"
+
+
+  print "python ", sys.argv[0], " /storage/data/cms/users/llbb/productionJune2012_444/MCwithMatching/Fall11_ZHbb_125/  11 _ZH125ee"
+  print "python ", sys.argv[0], " /storage/data/cms/users/llbb/productionJune2012_444/MCwithMatching/Fall11_ZHbb_125/  11 _ZH125mumu"
+
+  print "python ", sys.argv[0], " /storage/data/cms/users/llbb/productionJune2012_444/MCwithMatching/Fall11_ZHbb_115/ 11 _ZHbb_115"
+  print "python ", sys.argv[0], " /storage/data/cms/users/llbb/productionJune2012_444/MCwithMatching/Fall11_ZHbb_120/ 11 _ZHbb_120"
+  print "python ", sys.argv[0], " /storage/data/cms/users/llbb/productionJune2012_444/MCwithMatching/Fall11_ZHbb_125/ 11 _ZHbb_125"
+  print "python ", sys.argv[0], " /storage/data/cms/users/llbb/productionJune2012_444/MCwithMatching/Fall11_ZHbb_130/ 11 _ZHbb_130"
+  print "python ", sys.argv[0], " /storage/data/cms/users/llbb/productionJune2012_444/MCwithMatching/Fall11_ZHbb_135/ 11 _ZHbb_135"
+
   exit()
 
 path=sys.argv[1]
-njobs=int(sys.argv[2])
+#njobs=int(sys.argv[2])
+nfilespersection=int(sys.argv[2])
 suffix=sys.argv[3]
 
 FarmDirectory = "FARM_CMSToLHCO"+suffix
@@ -41,8 +62,9 @@ dirList=os.listdir(path)
 
 #number of rootfiles
 nrootfile=len(dirList)
-nexact=(nrootfile%njobs==0)
+njobs = int(nrootfile)/nfilespersection
 
+nexact=(nrootfile%njobs==0)
 
 
 if njobs > nrootfile:
@@ -66,7 +88,7 @@ LaunchOnCondor.Jobs_RunHere= 1
 
 for i in range(0,njobs):
    #command = path isection Nfilessection suffix 
-   command = "./CMS_To_LHCO_usingFramework_batch.py" + " " + path + " " + str(i) + " " + str(int(nrootfile/njobs)) + " " + suffix
+   command = "./CMS_To_LHCO_usingFramework_batch.py" + " " + path + " " + str(i) + " " + str(int(nfilespersection)) + " " + suffix
 
    #print "command = ", command
    LaunchOnCondor.SendCluster_Push(["PYTHON", command])
