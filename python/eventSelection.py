f971c3c5b16216db9679b99cfee24d6d8036cba5
Adrien Caudron <adrien.caudron@cern.ch>
2012-12-06 17:16:26 +0000

    sort jet indices by CSV discriminant value, to select the 2 highest one

--- python/eventSelection.py
+++ python/eventSelection.py
@@ -497,6 +497,14 @@ def findDijetPair(jets, bestZcandidate=None, btagging="SSV"):
   """Find the best jet pair: high Pt and btagging."""
   # check number of good jets
   indices = [index for index,jet in enumerate(jets) if isGoodJet(jet,bestZcandidate) ]
+
+  if not isZbbSelection:
+    csvList = [(jet.bDiscriminator("combinedSecondaryVertexBJetTags"),index) for index,jet in enumerate(jets) if isGoodJet(jet,bestZcandidate)]
+    csvList.sort(reverse=True)
+    indices = []
+    for icsv in csvList:
+      indices.append(icsv[1])
+  
   if len(indices)<1: return (None, None)
   if len(indices)<2: return (jets[indices[0]],None)
   jetList = []
@@ -505,17 +513,28 @@ def findDijetPair(jets, bestZcandidate=None, btagging="SSV"):
     if isBJet(jets[index],"HP",btagging):
       jetList.append(index)
       indices.remove(index)
-  if len(jetList)>=2: return (jets[jetList[0]],jets[jetList[1]])
+  if len(jetList)>=2:
+    if jets[jetList[0]].pt()>jets[jetList[1]] :
+      return (jets[jetList[0]],jets[jetList[1]])
+    else :
+      return (jets[jetList[1]],jets[jetList[0]])
   # continue with HE b-jets
   for index in indices[:]:
     if isBJet(jets[index],"HE",btagging):
       jetList.append(index)
       indices.remove(index)
-  if len(jetList)>=2: return (jets[jetList[0]],jets[jetList[1]])
+  if len(jetList)>=2:
+    if jets[jetList[0]].pt()>jets[jetList[1]] :
+      return (jets[jetList[0]],jets[jetList[1]])
+    else :
+      return (jets[jetList[1]],jets[jetList[0]])
   # fill with remaining good jets
   for index in indices:
     jetList.append(index)
-  return (jets[jetList[0]],jets[jetList[1]])
+  if jets[jetList[0]].pt()>jets[jetList[1]] :
+    return (jets[jetList[0]],jets[jetList[1]])
+  else :
+    return (jets[jetList[1]],jets[jetList[0]])
 
 categoryNames = [ 
   "Trigger", 
