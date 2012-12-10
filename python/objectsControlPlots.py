7dc943a109abbfd4faf2fa942ab986bf07ccbfdc
Jesus Vizan <Jesus.Manuel.Vizan.Garcia@cern.ch>
2012-12-10 11:55:39 +0000

    Correct the logic for filling the bjet{1,2} variables. It was not working well for the CSV selection.

--- python/objectsControlPlots.py
+++ python/objectsControlPlots.py
@@ -373,6 +373,7 @@ class JetmetControlPlots(BaseControlPlots):
       nj  = 0
       nb  = 0
       nbP = 0
+      indexDijet = 0
       maxbdiscSSVHE = -1
       maxbdiscSSVHP = -1
       maxbdiscCSV  = -1
@@ -464,7 +465,9 @@ class JetmetControlPlots(BaseControlPlots):
             result["jet2betaStar"] = jet.userFloat("betaStar")
           if isBJet(jet,"HE",self.btagging): 
             nb += 1
-            if nb==1 and ( isZbbSelection or (not isZbbSelection and jet in dijet) ) :
+	  if isBJet(jet,"HE",self.btagging) and ( isZbbSelection or (not isZbbSelection and jet in dijet) ) :
+	    indexDijet += 1
+            if indexDijet==1:
               result["bjet1pt"] = jetPt
 	      result["bjet1pt_totunc"] = self._JECuncertainty.unc_tot_jet(jet)
 	      result["bjet1Flavor"] = jet.partonFlavour()
@@ -483,7 +486,7 @@ class JetmetControlPlots(BaseControlPlots):
 	      result["dptj1b1"] = jetPt-j1pt
               result["bjet1beta"] = jet.userFloat("beta")
               result["bjet1betaStar"] = jet.userFloat("betaStar")
-            elif nb==2 and ( isZbbSelection or (not isZbbSelection and jet in dijet) ) :
+            elif indexDijet==2:
               result["bjet2pt"] = jetPt
 	      result["bjet2pt_totunc"] = self._JECuncertainty.unc_tot_jet(jet)
 	      result["bjet2Flavor"] = jet.partonFlavour()
