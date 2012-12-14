ecdbdad12f478c8804be96a316967cabac42e9d5
Jesus Vizan <Jesus.Manuel.Vizan.Garcia@cern.ch>
2012-12-14 19:09:14 +0000

    Updated the launcher for the version of CMStoLHCO to be used for the 2011 CSV selection

--- python/Launch_CMS_To_LHCO.py
+++ python/Launch_CMS_To_LHCO.py
@@ -16,53 +16,86 @@ if narguments != 4:
   print "Usage: ", sys.argv[0], " foldername njobs suffix"
   print "Examples"
   print "python ", sys.argv[0], " /storage/data/cms/users/llbb/productionJune2012_444/MCwithMatching/zbbProd/ 3 _Zbb4Flavee"
-  print "python ", sys.argv[0], " /storage/data/cms/users/llbb/productionJune2012_444/MCwithMatching/zbbProd/ 3 _Zbb4Flavmumu"
 
 
-  print "python ", sys.argv[0], " /storage/data/cms/users/llbb/productionJune2012_444/MCwithMatching/zbbProd/ 3 _Zbb4Flavmumu"
-
-  print "python ", sys.argv[0], " /storage/data/cms/users/llbb/productionJune2012_444/MCwithMatching/Fall11_TTbar_v3/ 54 _TTmumu"
-  print "python ", sys.argv[0], " /storage/data/cms/users/llbb/productionJune2012_444/MCwithMatching/Fall11_TTbar_v3/ 54 _TTee"
-
-  print "python ", sys.argv[0], " /home/fynu/vizangarciaj/scratch/DATA_5fb_EleA/ 499 _DataEleA"
-  print "python ", sys.argv[0], " /home/fynu/vizangarciaj/scratch/DATA_5fb_EleB/ 353 _DataEleB"
-
-  print "python ", sys.argv[0], " /home/fynu/vizangarciaj/scratch/DATA_5fb_Mu/ 183 _DataMu"
-
-  print "python ", sys.argv[0], " /home/fynu/vizangarciaj/storage/DY444secondProdAllButMissingFile/ 1451 _DYmumu"
-  print "python ", sys.argv[0], " /home/fynu/vizangarciaj/storage/DY444secondProdAllButMissingFile/ 1451 _DYee"
-
-  print "python ", sys.argv[0], " /storage/data/cms/store/user/acaudron/Fall11MC_444/zbbProd/ 3 _Zbb4flveeNew"
-  print "python ", sys.argv[0], " /storage/data/cms/store/user/acaudron/Fall11MC_444/zbbProd/ 3 _Zbb4flvmumuNew"
-
-  print "python ", sys.argv[0], " /storage/data/cms/store/user/acaudron/Fall11MC_444/Fall11_DYjets_v4/acaudron/DYJetsToLL_TuneZ2_M-50_7TeV-madgraph-tauola/production5fb_June2012_Fall11_DYjets_v4/918089973f5ad18b0c79d580ed9142aa/ 66 _DYmumu"
-  print "python ", sys.argv[0], " /storage/data/cms/store/user/acaudron/Fall11MC_444/Fall11_DYjets_v4/acaudron/DYJetsToLL_TuneZ2_M-50_7TeV-madgraph-tauola/production5fb_June2012_Fall11_DYjets_v4/918089973f5ad18b0c79d580ed9142aa/ 66 _DYee"
-
-
-  print "python ", sys.argv[0], " /storage/data/cms/users/llbb/productionJune2012_444/MCwithMatching/Fall11_ZHbb_125/  11 _ZH125ee"
-  print "python ", sys.argv[0], " /storage/data/cms/users/llbb/productionJune2012_444/MCwithMatching/Fall11_ZHbb_125/  11 _ZH125mumu"
-
-  print "python ", sys.argv[0], " /storage/data/cms/users/llbb/productionJune2012_444/MCwithMatching/Fall11_ZHbb_115/ 11 _ZHbb_115"
-  print "python ", sys.argv[0], " /storage/data/cms/users/llbb/productionJune2012_444/MCwithMatching/Fall11_ZHbb_120/ 11 _ZHbb_120"
-  print "python ", sys.argv[0], " /storage/data/cms/users/llbb/productionJune2012_444/MCwithMatching/Fall11_ZHbb_125/ 11 _ZHbb_125"
-  print "python ", sys.argv[0], " /storage/data/cms/users/llbb/productionJune2012_444/MCwithMatching/Fall11_ZHbb_130/ 11 _ZHbb_130"
-  print "python ", sys.argv[0], " /storage/data/cms/users/llbb/productionJune2012_444/MCwithMatching/Fall11_ZHbb_135/ 11 _ZHbb_135"
+  print "python ", sys.argv[0], "MuA_DATA 9999 _CSV2011Sel"
+  print "python ", sys.argv[0], "ElA_DATA 9999 _CSV2011Sel"
+  print "python ", sys.argv[0], "MuB_DATA 9999 _CSV2011Sel"
+  print "python ", sys.argv[0], "ElB_DATA 9999 _CSV2011Sel"
+  print "python ", sys.argv[0], "Mu_MC 9999 _CSV2011Sel"
+  print "python ", sys.argv[0], "El_MC 9999 _CSV2011Sel"
+  print "python ", sys.argv[0], "TT_Mu_MC 9999 _CSV2011Sel"
+  print "python ", sys.argv[0], "TT_El_MC 9999 _CSV2011Sel"
+  print "python ", sys.argv[0], "ZZ_Mu_MC 9999 _CSV2011Sel"
+  print "python ", sys.argv[0], "ZZ_El_MC 9999 _CSV2011Sel"
+  print "python ", sys.argv[0], "ZH115_Mu_MC 9999 _CSV2011Sel"
+  print "python ", sys.argv[0], "ZH115_El_MC 9999 _CSV2011Sel"
+  print "python ", sys.argv[0], "ZH120_Mu_MC 9999 _CSV2011Sel"
+  print "python ", sys.argv[0], "ZH120_El_MC 9999 _CSV2011Sel"
+  print "python ", sys.argv[0], "ZH125_Mu_MC 9999 _CSV2011Sel"
+  print "python ", sys.argv[0], "ZH125_El_MC 9999 _CSV2011Sel"
+  print "python ", sys.argv[0], "ZH130_Mu_MC 9999 _CSV2011Sel"
+  print "python ", sys.argv[0], "ZH130_El_MC 9999 _CSV2011Sel"
+  print "python ", sys.argv[0], "ZH135_Mu_MC 9999 _CSV2011Sel"
+  print "python ", sys.argv[0], "ZH135_El_MC 9999 _CSV2011Sel"
 
   exit()
+  
+mappath = { 
+  "MuA_DATA"     : "/nfs/user/acaudron/skim444/Mu_DataA/" ,
+  "ElA_DATA"     : "/nfs/user/acaudron/skim444/El_DataA/" ,
+  "MuB_DATA"     : "/nfs/user/acaudron/skim444/Mu_DataB/" ,
+  "ElB_DATA"     : "/nfs/user/acaudron/skim444/El_DataB/" ,
+  "Mu_MC"        : "/nfs/user/acaudron/skim444/DY_MC/"    ,
+  "El_MC"        : "/nfs/user/acaudron/skim444/DY_MC/"    ,
+  #"Mu_MC"        : "/storage/data/cms/store/user/acaudron/Torino/DYJets_MCMatched_00.root"    , 
+  #"El_MC"        : "/storage/data/cms/store/user/acaudron/Torino/DYJets_MCMatched_00.root"    , 
+  "Zbb_Mu_MC"    : "/storage/data/cms/store/user/acaudron/Fall11MC_444/zbbProd/"   ,
+  "Zbb_El_MC"    : "/storage/data/cms/store/user/acaudron/Fall11MC_444/zbbProd/"   ,
+  "TT_Mu_MC"     : "/nfs/user/acaudron/skim444/TT_MC/"    ,
+  "TT_El_MC"     : "/nfs/user/acaudron/skim444/TT_MC/"    ,
+  "ZZ_Mu_MC"     : "/nfs/user/acaudron/skim444/ZZ_MC/"    ,
+  "ZZ_El_MC"     : "/nfs/user/acaudron/skim444/ZZ_MC/"    ,
+  "ZH115_Mu_MC"  : "/nfs/user/acaudron/skim444/ZH115_MC/" ,
+  "ZH115_El_MC"  : "/nfs/user/acaudron/skim444/ZH115_MC/" ,
+  "ZH120_Mu_MC"  : "/nfs/user/acaudron/skim444/ZH120_MC/" ,
+  "ZH120_El_MC"  : "/nfs/user/acaudron/skim444/ZH120_MC/" ,
+  "ZH125_Mu_MC"  : "/nfs/user/acaudron/skim444/ZH125_MC/" ,
+  "ZH125_El_MC"  : "/nfs/user/acaudron/skim444/ZH125_MC/" ,
+  "ZH130_Mu_MC"  : "/nfs/user/acaudron/skim444/ZH130_MC/" ,
+  "ZH130_El_MC"  : "/nfs/user/acaudron/skim444/ZH130_MC/" ,
+  "ZH135_Mu_MC"  : "/nfs/user/acaudron/skim444/ZH135_MC/" ,
+  "ZH135_El_MC"  : "/nfs/user/acaudron/skim444/ZH135_MC/" ,
+  "tW_Mu_MC"     : "/nfs/user/acaudron/skim444/tW_MC/"    ,
+  "tW_El_MC"     : "/nfs/user/acaudron/skim444/tW_MC/"    ,
+  "tbarW_Mu_MC"  : "/nfs/user/acaudron/skim444/tbarW_MC/" ,
+  "tbarW_El_MC"  : "/nfs/user/acaudron/skim444/tbarW_MC/" ,
+  "evtgen_MC"    : "/storage/data/cms/users/tdupree/zbb_2011/evtgen/",
+  "herwig_MC"    : "/storage/data/cms/users/tdupree/zbb_2011/herwig/",
+  "pythia_MC"    : "/storage/data/cms/users/tdupree/zbb_2011/pythia/",
+  "ZA_Mu_MC" : "/nfs/user/acaudron/ZApat/",
+  "ZA_El_MC" : "/nfs/user/acaudron/ZApat/"
+  }
 
-path=sys.argv[1]
-#njobs=int(sys.argv[2])
-nfilespersection=int(sys.argv[2])
+
+channel = sys.argv[1]
 suffix=sys.argv[3]
+nfilespersection=int(sys.argv[2])
 
-FarmDirectory = "FARM_CMSToLHCO"+suffix
+path=mappath[channel]
+
+FarmDirectory = "FARM_CMSToLHCO_"+channel+suffix
 
 #dir with the number of rootfiles
 dirList=os.listdir(path)
 
+
 #number of rootfiles
 nrootfile=len(dirList)
 njobs = int(nrootfile)/nfilespersection
+if  nfilespersection > nrootfile: njobs = 1
+
+#print "dirList=", dirList, " nrootfile=",nrootfile, " nfilespersection = ", nfilespersection, " njobs=",njobs
 
 nexact=(nrootfile%njobs==0)
 
@@ -80,7 +113,7 @@ else :
   
 print "I will run over the rootfiles in path = ", path, " which contains ", nrootfile, "rootfiles"
 
-JobName = "outCMStoLHCO"+suffix
+JobName = "outCMStoLHCO_"+channel+suffix
 LaunchOnCondor.Jobs_RunHere = 1
 LaunchOnCondor.SendCluster_Create(FarmDirectory, JobName)
 LaunchOnCondor.Jobs_RunHere= 1
@@ -88,7 +121,7 @@ LaunchOnCondor.Jobs_RunHere= 1
 
 for i in range(0,njobs):
    #command = path isection Nfilessection suffix 
-   command = "./CMS_To_LHCO_usingFramework_batch.py" + " " + path + " " + str(i) + " " + str(int(nfilespersection)) + " " + suffix
+   command = "./CMS_To_LHCO_usingFramework_batch.py" + " " + channel + " " + str(i) + " " + str(int(nfilespersection)) + " " + suffix
 
    #print "command = ", command
    LaunchOnCondor.SendCluster_Push(["PYTHON", command])
