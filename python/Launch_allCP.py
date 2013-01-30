a62604e3966594410be3426a92e1a7afaacab215
Adrien Caudron <adrien.caudron@cern.ch>
2013-01-30 15:34:52 +0000

    new version with all 2012 datasets

--- python/Launch_allCP.py
+++ python/Launch_allCP.py
@@ -9,91 +9,125 @@ import glob
 import os
 
 samples = [
+    "DY",
     "TT",
-    "Zl",
-    "Zc",
-    "Zb",
     "ZZ",
     "ZH",
-    "Ele",
-    "Mu"
     ]
+
+DYsamples = [
+    "DYjets",
+    "DY1jets",
+    "DY2jets",
+    "DY3jets",
+    "DY4jets",
+    "DYjets_Pt50to70",
+    "DYjets_Pt70to100",
+    "DYjets_Pt180",
+    ]
+
+DYbcl = [
+    "Zb",
+    "Zc",
+    "Zl",
+    ]
+
+mass = [125] #[110,115,120,125,130,135]
 
 MC = "Summer12"
 DATA = "2012"
-cpVersion = "V14"
+cpVersion = "V1"
 
 listdata=[
         "A",
-        "B"
+        "A06aug",
+        "B",
+        "C-v1",
+        "C-v2",
+        "D",
         ]
+
+DataChannel = [
+    "Ele",
+    "Mu",
+    ]
+
+DataSample = [
+    #"Single",
+    "Double",
+    ]
+
+jobs = {
+    "A" : 50,
+    "A06aug" : 10,
+    "B" : 250,
+    "C-v1" : 25,
+    "C-v2" : 300,
+    "D" : 400,  
+    "TT" : 150,
+    "ZZ" : 50,
+    "ZH" : 50,
+    "DYjets" : 400,
+    "DY1jets" : 300,
+    "DY2jets" : 200,
+    "DY3jets" : 150,
+    "DY4jets" : 100,
+    "DYjets_Pt50to70" : 300,
+    "DYjets_Pt70to100" : 200,
+    "DYjets_Pt180" : 100,
+    }
 
 os.system('mkdir ControlPlots_'+cpVersion)
 for sample in samples :
     if sample=="ZH":
-        mass = [125] #[115,120,125,130,135]
         for m in mass:
             os.system('mkdir ControlPlots_'+cpVersion+'/ControlPlots_'+sample+str(m))
-    elif sample=="Ele" or sample=="Mu":
-        for l in listdata:
-            os.system('mkdir ControlPlots_'+cpVersion+'/ControlPlots_'+sample+DATA+l)
+    elif sample=="DY":
+        for dy in DYsamples :
+            for fl in DYbcl :
+                os.system('mkdir ControlPlots_'+cpVersion+'/ControlPlots_'+dy+'_'+fl)
     else: os.system('mkdir ControlPlots_'+cpVersion+'/ControlPlots_'+sample)
 
+for ch in DataChannel :
+    for samp in DataSample :
+        for period in listdata :
+            os.system('mkdir ControlPlots_'+cpVersion+'/ControlPlots_'+samp+ch+DATA+period)
+
 FarmDirectory = "FARM_CP_"+cpVersion
-JobName = "ZbbAnalysis_All"
+JobName = "llbb_All_"+cpVersion
 LaunchOnCondor.Jobs_RunHere = 1
 LaunchOnCondor.SendCluster_Create(FarmDirectory, JobName)
 LaunchOnCondor.Jobs_RunHere= 1
+
 if "TT" in samples :
-    njobs = 350
+    njobs = jobs["TT"]
     for i in range(0,njobs):
-        LaunchOnCondor.SendCluster_Push(["PYTHON", os.getcwd()+"/ControlPlots.py -i /nfs/user/acaudron/TTjetsMassiveB_Summer12_S10/ -o ControlPlots_"+cpVersion+"/ControlPlots_TT/TT_"+MC+"_"+str(i)+".root --all -p ../data/Cert_190456-196531_8TeV_13Jul2012ReReco_Collisions12_JSON_pileupTruth.root -P ../data/MCpileup_Summer12_S10.root -w ../data/performance_csv_witheff.root  --Njobs "+str(njobs)+" --jobNumber "+str(i)])
-
-if "Zl" in samples :
-    njobs = 400
-    for i in range(0,njobs):
-        LaunchOnCondor.SendCluster_Push(["PYTHON", os.getcwd()+"/ControlPlots.py -i /storage/data/cms/store/user/acaudron/Sept2012production8TeV/Summer12_DYjets_S10_V2/acaudron/DYJetsToLL_M-50_TuneZ2Star_8TeV-madgraph-tarball/Sept2012production8TeV_Summer12_DYjets_S10_V2/d5186fd016990c766c736d42e5dce079/ -o ControlPlots_"+cpVersion+"/ControlPlots_Zl/Zl_"+MC+"_"+str(i)+".root --all -j l -p ../data/Cert_190456-196531_8TeV_13Jul2012ReReco_Collisions12_JSON_pileupTruth.root -P ../data/MCpileup_Summer12_S10.root -w ../data/performance_csv_witheff.root  --Njobs "+str(njobs)+" --jobNumber "+str(i)])
-
-if "Zc" in samples :
-    for i in range(0,njobs):
-        LaunchOnCondor.SendCluster_Push(["PYTHON", os.getcwd()+"/ControlPlots.py -i /storage/data/cms/store/user/acaudron/Sept2012production8TeV/Summer12_DYjets_S10_V2/acaudron/DYJetsToLL_M-50_TuneZ2Star_8TeV-madgraph-tarball/Sept2012production8TeV_Summer12_DYjets_S10_V2/d5186fd016990c766c736d42e5dce079/ -o ControlPlots_"+cpVersion+"/ControlPlots_Zc/Zc_"+MC+"_"+str(i)+".root --all -j c -p ../data/Cert_190456-196531_8TeV_13Jul2012ReReco_Collisions12_JSON_pileupTruth.root -P ../data/MCpileup_Summer12_S10.root -w ../data/performance_csv_witheff.root  --Njobs "+str(njobs)+" --jobNumber "+str(i)])
-
-if "Zb" in samples :
-    for i in range(0,njobs):
-        LaunchOnCondor.SendCluster_Push(["PYTHON", os.getcwd()+"/ControlPlots.py -i /storage/data/cms/store/user/acaudron/Sept2012production8TeV/Summer12_DYjets_S10_V2/acaudron/DYJetsToLL_M-50_TuneZ2Star_8TeV-madgraph-tarball/Sept2012production8TeV_Summer12_DYjets_S10_V2/d5186fd016990c766c736d42e5dce079/ -o ControlPlots_"+cpVersion+"/ControlPlots_Zb/Zb_"+MC+"_"+str(i)+".root --all -j b -p ../data/Cert_190456-196531_8TeV_13Jul2012ReReco_Collisions12_JSON_pileupTruth.root -P ../data/MCpileup_Summer12_S10.root -w ../data/performance_csv_witheff.root  --Njobs "+str(njobs)+" --jobNumber "+str(i)])
+        LaunchOnCondor.SendCluster_Push(["PYTHON", os.getcwd()+"/ControlPlots.py -i /nfs/user/llbb/Pat_8TeV_537/TTjets_Summer12_S10/ -o ControlPlots_"+cpVersion+"/ControlPlots_TT/TT_"+MC+"_"+str(i)+".root --all -p ../data/Cert_190456-208686_8TeV_PromptPlusReReco_pileupTruth.root -P ../data/MCpileup_Summer12_S10.root -w ../data/performance_csv_witheff.root  --Njobs "+str(njobs)+" --jobNumber "+str(i)])
 
 if "ZZ" in samples :
-    njobs = 50
+    njobs = jobs["ZZ"]
     for i in range(0,njobs):
-        LaunchOnCondor.SendCluster_Push(["PYTHON", os.getcwd()+"/ControlPlots.py -i /storage/data/cms/store/user/acaudron/Sept2012production8TeV/Summer12_ZZ_S10_V2/acaudron/ZZ_TuneZ2star_8TeV_pythia6_tauola/Sept2012production8TeV_Summer12_ZZ_S10_V2/d5186fd016990c766c736d42e5dce079/ -o ControlPlots_"+cpVersion+"/ControlPlots_ZZ/ZZ_"+MC+"_"+str(i)+".root --all -p ../data/Cert_190456-196531_8TeV_13Jul2012ReReco_Collisions12_JSON_pileupTruth.root -P ../data/MCpileup_Summer12_S10.root -w ../data/performance_csv_witheff.root  --Njobs "+str(njobs)+" --jobNumber "+str(i)])
+        LaunchOnCondor.SendCluster_Push(["PYTHON", os.getcwd()+"/ControlPlots.py -i /nfs/user/llbb/Pat_8TeV_537/Summer12_ZZ_S10/ -o ControlPlots_"+cpVersion+"/ControlPlots_ZZ/ZZ_"+MC+"_"+str(i)+".root --all -p ../data/Cert_190456-208686_8TeV_PromptPlusReReco_pileupTruth.root -P ../data/MCpileup_Summer12_S10.root -w ../data/performance_csv_witheff.root  --Njobs "+str(njobs)+" --jobNumber "+str(i)])
 
-if "Ele" in samples :
-    njobs = 50
-    if "A" in listdata :
-        for i in range(0,njobs):
-            LaunchOnCondor.SendCluster_Push(["PYTHON", os.getcwd()+"/ControlPlots.py -i /storage/data/cms/store/user/acaudron/Sept2012production8TeV/Ele2012A_V4/acaudron/DoubleElectron/Sept2012production8TeV_Ele2012A_V4/ee8b79c5bec180099e91bf02c7018cc0/ -o ControlPlots_"+cpVersion+"/ControlPlots_Ele"+DATA+"A/Ele"+DATA+"A_"+MC+"_"+str(i)+".root --all --noPUweight --noBweight --noLweight --trigger --onlyEle --Njobs "+str(njobs)+" --jobNumber "+str(i)])
+for ch in DataChannel :
+    for samp in DataSample :
+        for period in listdata :
+            njobs = jobs[period]
+            for i in range(0,njobs):
+                LaunchOnCondor.SendCluster_Push(["PYTHON", os.getcwd()+"/ControlPlots.py -i /nfs/user/llbb/Pat_8TeV_537/"+samp+ch+DATA+period+"/ -o ControlPlots_"+cpVersion+"/ControlPlots_"+samp+ch+DATA+period+"/"+samp+ch+DATA+period+"_"+str(i)+".root --all --noPUweight --noBweight --noLweight --trigger --onlyMu --Njobs "+str(njobs)+" --jobNumber "+str(i)])
 
-    njobs = 300
-    if "B" in listdata :
-        for i in range(0,njobs):
-            LaunchOnCondor.SendCluster_Push(["PYTHON", os.getcwd()+"/ControlPlots.py -i /storage/data/cms/store/user/acaudron/Sept2012production8TeV/Ele2012B_V4/acaudron/DoubleElectron/Sept2012production8TeV_Ele2012B_V4/ee8b79c5bec180099e91bf02c7018cc0/ -o ControlPlots_"+cpVersion+"/ControlPlots_Ele"+DATA+"B/Ele"+DATA+"B_"+MC+"_"+str(i)+".root --all --noPUweight --noBweight --noLweight --trigger --onlyEle --Njobs "+str(njobs)+" --jobNumber "+str(i)])
-
-if "Mu" in samples :
-    njobs = 50
-    if "A" in listdata :
-        for i in range(0,njobs):
-            LaunchOnCondor.SendCluster_Push(["PYTHON", os.getcwd()+"/ControlPlots.py -i /storage/data/cms/store/user/acaudron/Sept2012production8TeV/Mu2012A_V3/acaudron/DoubleMu/Sept2012production8TeV_Mu2012A_V3/18ff4149fdd30a1229c72782767e6503/ -o ControlPlots_"+cpVersion+"/ControlPlots_Mu"+DATA+"A/Mu"+DATA+"A_"+MC+"_"+str(i)+".root --all --noPUweight --noBweight --noLweight --trigger --onlyMu --Njobs "+str(njobs)+" --jobNumber "+str(i)])
-
-    njobs = 300
-    if "B" in listdata :
-        for i in range(0,njobs):
-            LaunchOnCondor.SendCluster_Push(["PYTHON", os.getcwd()+"/ControlPlots.py -i /storage/data/cms/store/user/acaudron/Sept2012production8TeV/Mu2012B_V3/acaudron/DoubleMu/Sept2012production8TeV_Mu2012B_V3/18ff4149fdd30a1229c72782767e6503/ -o ControlPlots_"+cpVersion+"/ControlPlots_Mu"+DATA+"B/Mu"+DATA+"B_"+MC+"_"+str(i)+".root --all --noPUweight --noBweight --noLweight --trigger --onlyMu --Njobs "+str(njobs)+" --jobNumber "+str(i)])
+if "DY" in samples :
+    for dy in DYsamples :
+        njobs = jobs[dy]        
+        for fl in DYbcl :
+            for i in range(0,njobs):
+                LaunchOnCondor.SendCluster_Push(["PYTHON", os.getcwd()+"/ControlPlots.py -i /nfs/user/llbb/Pat_8TeV_537/Summer12_"+dy+"_S10/ -o ControlPlots_"+cpVersion+"/ControlPlots_"+dy+"_"+fl+"/"+dy+"_"+fl+"_"+MC+"_"+str(i)+".root --all -j "+fl[1]+" -p ../data/Cert_190456-208686_8TeV_PromptPlusReReco_pileupTruth.root -P ../data/MCpileup_Summer12_S10.root -w ../data/performance_csv_witheff.root  --Njobs "+str(njobs)+" --jobNumber "+str(i)])
 
 if "ZH" in samples :
     mass = [125]#[115,120,125,130,135]
     for m in mass:
-        njobs = 40
+        njobs = jobs["ZH"]
         for i in range(0,njobs):
-            LaunchOnCondor.SendCluster_Push(["PYTHON", os.getcwd()+"/ControlPlots.py -i /storage/data/cms/store/user/acaudron/Sept2012production8TeV/Summer12_ZH125_S10_V2/acaudron/ZH_ZToLL_HToBB_M-125_8TeV-powheg-herwigpp/Sept2012production8TeV_Summer12_ZH125_S10_V2/d5186fd016990c766c736d42e5dce079/ -o ControlPlots_"+cpVersion+"/ControlPlots_ZH"+str(m)+"/ZH"+str(m)+"_"+MC+"_"+str(i)+".root --all -p ../data/Cert_190456-196531_8TeV_13Jul2012ReReco_Collisions12_JSON_pileupTruth.root -P ../data/MCpileup_Summer12_S10.root -w ../data/performance_csv_witheff.root  --Njobs "+str(njobs)+" --jobNumber "+str(i)])
+            LaunchOnCondor.SendCluster_Push(["PYTHON", os.getcwd()+"/ControlPlots.py -i /nfs/user/llbb/Pat_8TeV_537/Summer12_ZH"+str(m)+"_S10/ -o ControlPlots_"+cpVersion+"/ControlPlots_ZH"+str(m)+"/ZH"+str(m)+"_"+MC+"_"+str(i)+".root --all -p ../data/Cert_190456-208686_8TeV_PromptPlusReReco_pileupTruth.root -P ../data/MCpileup_Summer12_S10.root -w ../data/performance_csv_witheff.root  --Njobs "+str(njobs)+" --jobNumber "+str(i)])
 
 LaunchOnCondor.SendCluster_Submit()
