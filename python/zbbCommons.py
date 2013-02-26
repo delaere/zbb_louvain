55e6d215b5bb1a00b681d469e65fb0d2a8bdf131
Camille Beluffi <camille.beluffi@cern.ch>
2013-02-26 13:07:18 +0000

    *** empty log message ***

--- python/zbbCommons.py
+++ python/zbbCommons.py
@@ -1,5 +1,5 @@
 ##choose if you want to thz Zbb selection or the ZHbb selection 
-isZbbSelection = False
+isZbbSelection = True
 tagger = "ssv_witheff_062012"
 if not isZbbSelection : tagger = "csv_witheff"
 
@@ -27,7 +27,7 @@ class zbbsystematics:
   SF_uncert="mean" ## choose among min/max/mean
   #SF_running_mode= "hardcoded" ## choose between hardcoded/database
   SF_running_mode= "database" ## choose between hardcoded/database
-  JERfactor = 0. # 1 = recommended smearing
+  JERfactor = 1. # 1 = recommended smearing
   JESfactor = 0. # 1 = +1sigma
   LeptonTnPfactor = 0
 
