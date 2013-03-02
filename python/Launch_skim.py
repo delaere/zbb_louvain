90a2cee9776776ee77a31de1eec9aaf7ee77ceac
Adrien Caudron <adrien.caudron@cern.ch>
2013-03-02 13:15:57 +0000

    improvement

--- python/Launch_skim.py
+++ python/Launch_skim.py
@@ -14,33 +14,22 @@ import sys
 import LaunchOnCondor
 import glob
 
-samples = [
-            "DY_MC",
-            "TT_MC",
-            "Mu_DataA",
-            "El_DataA",
-            "Mu_DataB",
-            "El_DataB",
-            "ZZ_MC",
-            #"ZH115_MC",
-            #"ZH120_MC",
-            "ZH125_MC",
-            #"ZH130_MC",
-            #"ZH135_MC"
-            ]
+from globalLists import listsamples, dirSkim
+samples = listsamples
 
-FarmDirectory = "FARM_skim"
+FarmDirectory = "FARM_skim_V5"
 JobName = "Skim_pats"
 LaunchOnCondor.Jobs_RunHere = 1
 LaunchOnCondor.SendCluster_Create(FarmDirectory, JobName)
 LaunchOnCondor.Jobs_RunHere= 1
 
+j=0
 for s in samples :
-    whichSample = s #options.which
-    os.system('mkdir /nfs/user/acaudron/skim53X/'+whichSample)
+    whichSample = s 
+    os.system('mkdir '+dirSkim+whichSample)
             
     for i in range(1,11):
-        command = "/nfs/user/acaudron/CMSSW_5_3_2_patch4/src/UserCode/zbb_louvain/test/skimDY_423_cfg.py"  
+        command = "../test/skimDY_423_cfg.py"  
         option  = " sample="+whichSample+" slice="+str(i)  
         print "command = ", command
         print "option  = ", option
