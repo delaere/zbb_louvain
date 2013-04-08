0a336dc73c9e81da72253b1e50f86597c54a1526
Adrien Caudron <adrien.caudron@cern.ch>
2013-04-08 09:05:35 +0000

    some fixes

--- scripts/ME_Analysis/MW_start_llbb_Louvain_Pack/bashScripts/modifyCards.sh
+++ scripts/ME_Analysis/MW_start_llbb_Louvain_Pack/bashScripts/modifyCards.sh
@@ -7,8 +7,8 @@ do
   cd ggZbb_${c}
   cp ../replace.py .
   python replace.py Cards/run_card.dat 7000 4000
-  python replace.py Cards/MadWeight_card.dat madgraph==True CMSFARM==True
-  python replace.py Cards/MadWeight_card.dat "cluster              0" "cluster              1"
+  #python replace.py Cards/MadWeight_card.dat madgraph==True CMSFARM==True
+  #python replace.py Cards/MadWeight_card.dat "cluster              0" "cluster              1"
   python replace.py Cards/MadWeight_card.dat "13       180" "13       172.5"
   python replace.py Cards/MadWeight_card.dat "13       190" "#13       190"
   python replace.py Cards/param_card.dat 1.730000e+02 1.725000e+02
@@ -17,8 +17,8 @@ do
   cd ../qqZbb_${c}
   cp ../replace.py .
   python replace.py Cards/run_card.dat 7000 4000
-  python replace.py Cards/MadWeight_card.dat madgraph==True CMSFARM==True
-  python replace.py Cards/MadWeight_card.dat "cluster              0" "cluster              1"
+  #python replace.py Cards/MadWeight_card.dat madgraph==True CMSFARM==True
+  #python replace.py Cards/MadWeight_card.dat "cluster              0" "cluster              1"
   python replace.py Cards/MadWeight_card.dat "13       180" "13       172.5"
   python replace.py Cards/MadWeight_card.dat "13       190" "#13       190"
   python replace.py Cards/param_card.dat 1.730000e+02 1.725000e+02
@@ -27,8 +27,8 @@ do
   cd ../ttbar_${c}
   cp ../replace.py .
   python replace.py Cards/run_card.dat 7000 4000
-  python replace.py Cards/MadWeight_card.dat madgraph==True CMSFARM==True
-  python replace.py Cards/MadWeight_card.dat "cluster              0" "cluster              1"
+  #python replace.py Cards/MadWeight_card.dat madgraph==True CMSFARM==True
+  #python replace.py Cards/MadWeight_card.dat "cluster              0" "cluster              1"
   python replace.py Cards/MadWeight_card.dat "isr                  0" "isr                  2"
   python replace.py Cards/MadWeight_card.dat "13       180" "13       172.5"
   python replace.py Cards/MadWeight_card.dat "13       190" "#13       190"
@@ -38,8 +38,8 @@ do
   cd ../ZZ_C0_${c}
   cp ../replace.py .
   python replace.py Cards/run_card.dat 7000 4000
-  python replace.py Cards/MadWeight_card.dat madgraph==True CMSFARM==True
-  python replace.py Cards/MadWeight_card.dat "cluster              0" "cluster              1"
+  #python replace.py Cards/MadWeight_card.dat madgraph==True CMSFARM==True
+  #python replace.py Cards/MadWeight_card.dat "cluster              0" "cluster              1"
   python replace.py Cards/MadWeight_card.dat "13       180" "13       172.5"
   python replace.py Cards/MadWeight_card.dat "13       190" "#13       190"
   python replace.py Cards/param_card.dat 1.730000e+02 1.725000e+02
@@ -48,8 +48,8 @@ do
   cd ../ZZ_C3_${c}
   cp ../replace.py .
   python replace.py Cards/run_card.dat 7000 4000
-  python replace.py Cards/MadWeight_card.dat madgraph==True CMSFARM==True
-  python replace.py Cards/MadWeight_card.dat "cluster              0" "cluster              1"
+  #python replace.py Cards/MadWeight_card.dat madgraph==True CMSFARM==True
+  #python replace.py Cards/MadWeight_card.dat "cluster              0" "cluster              1"
   python replace.py Cards/MadWeight_card.dat "isr                  0" "isr                  3"
   python replace.py Cards/MadWeight_card.dat "13       180" "13       172.5"
   python replace.py Cards/MadWeight_card.dat "13       190" "#13       190"
@@ -62,8 +62,8 @@ do
     cd ../ZH_C0_${h}_${c}
     cp ../replace.py .
     python replace.py Cards/run_card.dat 7000 4000
-    python replace.py Cards/MadWeight_card.dat madgraph==True CMSFARM==True
-    python replace.py Cards/MadWeight_card.dat "cluster              0" "cluster              1"
+    #python replace.py Cards/MadWeight_card.dat madgraph==True CMSFARM==True
+    #python replace.py Cards/MadWeight_card.dat "cluster              0" "cluster              1"
     python replace.py Cards/MadWeight_card.dat "12       6" "12       25"
     python replace.py Cards/MadWeight_card.dat "13       180" "13       125"
     python replace.py Cards/MadWeight_card.dat "13       190" "#13       190"
@@ -74,8 +74,8 @@ do
     cd ../ZH_C3_${h}_${c}
     cp ../replace.py .
     python replace.py Cards/run_card.dat 7000 4000
-    python replace.py Cards/MadWeight_card.dat madgraph==True CMSFARM==True
-    python replace.py Cards/MadWeight_card.dat "cluster              0" "cluster              1"
+    #python replace.py Cards/MadWeight_card.dat madgraph==True CMSFARM==True
+    #python replace.py Cards/MadWeight_card.dat "cluster              0" "cluster              1"
     python replace.py Cards/MadWeight_card.dat "isr                  0" "isr                  3"
     python replace.py Cards/MadWeight_card.dat "12       6" "12       25"
     python replace.py Cards/MadWeight_card.dat "13       180" "13       125"
@@ -85,6 +85,5 @@ do
     python replace.py Cards/param_card.dat 5.753088e-03 0.160000e+00 
     rm replace.py
   done
+  cd ../
 done
-
-cd ..
