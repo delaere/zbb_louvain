972572c5015ee7418c30548b1f10cc58a65950a8
Christophe Delaere <christophe.delaere@cern.ch>
2013-07-29 23:40:35 +0200

    Rework zbb_counter script to use AnalysisEvent
    Some more work is needed to remove code duplication with other scripts.
    The common genpart code should go in MonteCarloSelection.py, if not yet there.

--- python/analysisScripts/zbb_counter.py
+++ python/analysisScripts/zbb_counter.py
@@ -1,18 +1,11 @@
 #!/nfs/soft/cms/slc5_amd64_gcc434/cms/cmssw/CMSSW_4_2_7/external/slc5_amd64_gcc434/bin/python
-from itertools import combinations
-import ROOT
-from ROOT import TLorentzVector
-import numpy as n
-import sys
 import os
-from DataFormats.FWLite import Events, Handle
-from LumiReWeighting import LumiReWeighting
-from zbbCommons import zbblabel
-from math import *
-from ROOT import TFile, TTree, TH1F
-from array import array
+import CMSSW
+from ROOT import TLorentzVector
+from AnalysisEvent import AnalysisEvent
+import EventSelection
 
-
+#TODO: move Delta to a "utilities.py"
 def Delta(par1,par2):
   delta_phi=abs(par2.phi()-par1.phi())
   if delta_phi>pi:
@@ -20,86 +13,27 @@ def Delta(par1,par2):
   delta=sqrt((delta_phi)**2 + (par1.eta()-par2.eta())**2)
   return delta
 
-#---------------------------------- for RUNNING in batch------------------------------------------------------
-#def dumpAll(muChannel=True, path="/storage/data/cms/store/user/castello/Unfolding2012/DYJets_unfolding_test/",fileAll="output.out"):
-#def dumpAll(muChannel=True, path="/storage/data/cms/store/user/castello/Unfolding2012/DYJets_unfolding_noRecoBias/",fileAll="output.out"):
+def countEvents(path="", output="", muChannel=True, Njobs=1, jobNumber=1):
 
+  # inputs
+  if os.path.isdir(path):
+    dirList=list(itertools.islice(os.listdir(path), jobNumber, None, Njobs))
+    files=[]
+    for fname in dirList:
+      files.append(path+"/"+fname)
+  elif os.path.isfile(path):
+    files=[path]
+  else:
+    files=[]
 
-###############################################################
-##   out_file_INCL= open(fileAll,"w")
-##   dirList=os.listdir(path)
-##   files=[]
-##   dirList.sort()
-##   for fname in dirList:
-##       files.append(path+fname)
-##       print 'file names', path+fname      
-### for quick run######################
-#read input files
-#  files=[]  
-#  files.append(infile1)
-#  files.append(infile2)
-#  #files.append(infile3)
-
-###########################################################
-
-def dumpAll(stage=9, muChannel=True, path="/storage/data/cms/store/user/castello/Unfolding2012/DYJets_unfolding_noRecoBias_jetNoNuBSM/castello/DYJetsToLL_TuneZ2_M-50_7TeV-madgraph-tauola/unfolding2012_v2_DYJets_unfolding_noRecoBias_jetNoNuBSM/1954ed00d02907f647c9352bb10d836e/",fileAll=None, numb=None, output=None):
-  out_file_INCL= open(fileAll,"w")
-  dirList=os.listdir(path)
-  files=[]
-  number=0
-  numb = int(numb)
-  filemin = numb*1
-  filemax = numb*1 + 2
-  dirList.sort()
-  #print 'number', numb, '\n'
-  #print 'filemin', filemin,'\n'
-  #print 'filemax', filemax,'\n'
-  for fname in dirList:
-    number+=1
-    print 'file names', path+fname
-    if number>filemin and number<filemax:
-      files.append(path+fname)
-      out_file_INCL.write('File name: '+ path+fname +' \n')
-
+  # events
+  events = AnalysisEvent(files)
+  EventSelection.prepareAnalysisEvent(events)
       
-  events = Events (files)
-  jetlabel="cleanPatJets"
-  jetgenlabel="patJets"
-  jetalllabel="patJets"
+  # output
+  out_file_INCL= open(output,"w")
 
-  zeelabelAll="ZelAllelAll"
-  zmmlabelAll="ZmuAllmuAll"
-  
-  genpartlabel="prunedGen"
-  labelElectron = "matchedElectrons"
-  labelAllElectron = "allElectrons"
-  labelAllMuon = "allMuons"
-  labelMuon = "matchedMuons"
-  vertexLabel ="goodPV"
-  PULabel = "addPileupInfo"
-  #GenjetLabel= "prunedJets"
-  GenjetLabel= "prunedNoNuBSMJets" ## new jet collection from Andrew
-
-  jetHandle = Handle ("vector<pat::Jet>")
-  jetallHandle = Handle ("vector<pat::Jet>")
-  metHandle = Handle ("vector<pat::MET>")
-  zmuHandle = Handle ("vector<reco::CompositeCandidate>")
-  zeleHandle = Handle ("vector<reco::CompositeCandidate>")
-  zmuHandle = Handle ("vector<reco::CompositeCandidate>")
-  
-  zeleAllHandle = Handle ("vector<reco::CompositeCandidate>")
-  zmuAllHandle = Handle ("vector<reco::CompositeCandidate>")
-  
-  zeleHandle = Handle ("vector<reco::CompositeCandidate>")
-  genpartHandle =  Handle("std::vector<reco::GenParticle>")
-  muonHandle = Handle ("vector<pat::Muon>")
-  electronHandle = Handle ("vector<pat::Electron>")
-  PrimaryVertexHandle = Handle ("vector<reco::Vertex>")
-  AllelectronHandle = Handle ("vector<pat::Electron>")
-  AllmuonHandle = Handle ("vector<pat::Muon>")
-  genJetHandle = Handle ("vector<reco::GenJet>")
-  PUHandle= Handle("std::vector<PileupSummaryInfo>")
-  #---------------------------------------------------------------------------------------------
+  # counters
   gen_llbb=0
   gen_llbbacc=0
   NrGen1b=0
@@ -107,46 +41,15 @@ def dumpAll(stage=9, muChannel=True, path="/storage/data/cms/store/user/castello
   NrGenZj=0
   NrGenZ=0
   
-  #---------------------------------------------------------------------------------------------
   # Event loop
   event_num=0
   for event in events:
-   #if event_num > -1: 
+
+    # display some counter
     if event_num%1000==0:
       print 'Event', event_num  
     event_num+=1
-    event.getByLabel (genpartlabel,genpartHandle)
-    genparts = genpartHandle.product()
-    event.getByLabel (jetlabel,jetHandle)
-    event.getByLabel (jetalllabel,jetallHandle)
    
-    #event.getByLabel (PULabel,PUHandle)
-    #event.getByLabel (labelElectron,electronHandle)
-    #event.getByLabel (labelMuon,muonHandle)
-    #event.getByLabel (vertexLabel, PrimaryVertexHandle)
-    event.getByLabel (GenjetLabel,genJetHandle) #jetgenlabel,
-    #event.getByLabel (labelAllElectron,AllelectronHandle)
-    #event.getByLabel (labelAllMuon,AllmuonHandle)
-        
-    #PU= PUHandle.product()
-    jets = jetHandle.product()
-    
-    #vertices = PrimaryVertexHandle.product()
-    #muons = muonHandle.product()
-    #electrons = electronHandle.product()
-    #AllMu = AllmuonHandle.product()
-    #Allelec = AllelectronHandle.product()
-    #numberOfInteractions = PUHandle.product()[0].getPU_NumInteractions()
-    run = event.eventAuxiliary().run()
-    triggerInfo = None
-    GenJet= genJetHandle.product()
-
-    #--------------------------------------------------------------------------------------------
-    #                                      start work with GEN info
-    #--------------------------------------------------------------------------------------------
-
-    ## defined for each event
-    
     # boolean
     GenZ=False
     GenZAcc=False
@@ -163,10 +66,14 @@ def dumpAll(stage=9, muChannel=True, path="/storage/data/cms/store/user/castello
     GenZ2b=False
     GenZj=False
      
-    ## GEN particle MAIN loop (for extracting leptons and B-hadrons)
-    for part in genparts:
+    #TODO: it should be generalized and/or use the existing methods for unfolding (Jerome+Ludivine)
+    #          - method to find b hadrons
+    #          - method to find gen Z
+    #          - method to find b jets 
+    #          - method to do the isolation
+    ## GEN particle loop (for extracting leptons and B-hadrons)
+    for part in event.genParticles:
       id = abs(part.pdgId())
-      
       ## looking for a B-hadron candidate (meson)
       if (id>499 and id<600):
         hasBottomedDaughter = False
@@ -176,7 +83,6 @@ def dumpAll(stage=9, muChannel=True, path="/storage/data/cms/store/user/castello
             hasBottomedDaughter = True
         if hasBottomedDaughter == False:
            b_hadron+=[part]
-
       ## looking for a B-hadron candidate (baryon)
       if (id>4999 and id<6000):
         hasBottomedDaughter = False
@@ -186,14 +92,11 @@ def dumpAll(stage=9, muChannel=True, path="/storage/data/cms/store/user/castello
             hasBottomedDaughter = True
         if hasBottomedDaughter == False:
            b_hadron+=[part]
-        
       ## looking for a Z genparticle candidate in the event
       if id == 23 and part.status()==3 and GenZMass==False:
-        #print 'status', part.status()
         l0=part.daughter(0)
         l1=part.daughter(1)
         if (l0 and l1 and l0.status()==3 and l1.status()==3 and ((muChannel==True and abs(l0.pdgId())==13 and abs(l1.pdgId())==13) or ( muChannel==False and abs(l0.pdgId())==11 and abs(l1.pdgId())==11))):
-          
           GenZ=True
           if (muChannel==True and l0.pt()>20 and l1.pt()>20 and abs(l0.eta())<2.4 and abs(l1.eta())<2.4) or (muChannel==False and l0.pt()>20 and l1.pt()>20 and abs(l0.eta())<2.4 and abs(l1.eta())<2.4):
             GenZAcc=True
@@ -203,43 +106,37 @@ def dumpAll(stage=9, muChannel=True, path="/storage/data/cms/store/user/castello
             if l0.charge()<0:
               lep1=l0
               lep0=l1
-              
             l1c=TLorentzVector(lep0.px(),lep0.py(),lep0.pz(),lep0.energy())
             l2c=TLorentzVector(lep1.px(),lep1.py(),lep1.pz(),lep1.energy())
             invGen=(l1c+l2c).M()
-            #print 'invGenMass-->', invGen
             if invGen>76 and invGen<106:
               GenZMass=True
-
     if (GenZ and GenZAcc and GenZMass):
       ZGenOK=True
     else:
       ZGenOK=False
       noGen=True
     
-    #----------------------------------------------------------------------------------------------------------------
-    #                                      GEN jet collection matched with  B-hadron
-    #----------------------------------------------------------------------------------------------------------------
-
+    # some containers
     genjetAcc=[]
     genbjetAcc=[]
     genbjet=[]
     genbjetAll=[]
 
-    for gj in GenJet:
+    # GEN jet loop to locate jets
+    for gj in event.genJets:
       if gj.pt()>25 and abs(gj.eta())<2.1:
         if  ZGenOK==True and Delta(gj,lep0)>0.5 and Delta(gj,lep1)>0.5:
           genjetAcc+=[gj]
 
-
-    #print 'genJetAcc size= ', len(genjetAcc)  
     if ZGenOK==True and len(genjetAcc)>0 :
       GenZj=True
     
+    # GEN jet loop to locate b jets
     if len(b_hadron)>0:
       b1=[]
       d=9999.
-      for gj in GenJet:
+      for gj in event.genJets:
         if gj.pt()>25 and abs(gj.eta())<2.1:
            genbjetAcc+=[gj]
         oneB=False
@@ -254,12 +151,9 @@ def dumpAll(stage=9, muChannel=True, path="/storage/data/cms/store/user/castello
                 genbjet+=[gj]
           else: break
           
-    #----------------------------------------------------------------------------------------------------------------
-    #                                       ISOLATION from leptons
-    #----------------------------------------------------------------------------------------------------------------
-
     genbj_iso=[]
          
+    # jet isolation
     if ZGenOK==True and len(genbjet)>0 :
       for bj in genbjet :
         if Delta(bj,lep0)>0.5 and Delta(bj,lep1)>0.5:
@@ -275,6 +169,7 @@ def dumpAll(stage=9, muChannel=True, path="/storage/data/cms/store/user/castello
     else:
       noGen=True
 
+    # count
     if(ZGenOK==True):
       NrGenZ+=1
     if(GenZj==True):
@@ -284,7 +179,7 @@ def dumpAll(stage=9, muChannel=True, path="/storage/data/cms/store/user/castello
     if(GenZ2b==True):  
       NrGen2b+=1 
   
-##### closing file ad dump count
+  ##### closing file ad dump count
   out_file_INCL.write('============================='+'\n')   
   out_file_INCL.write('total : '+ str(event_num) +' \n')
   out_file_INCL.write('Z_kin : '+ str(NrGenZ) +' \n')
@@ -294,12 +189,8 @@ def dumpAll(stage=9, muChannel=True, path="/storage/data/cms/store/user/castello
   out_file_INCL.write('============================'+' \n')   
   out_file_INCL.close()
 
+# run as a script
+if __name__=="__main__":
+  import sys
+  countEvents(sys.argv[1:]):
 
-## FOR RUNNING LOCALLY --------------------------
-##dumpAll()
-
-## FOR RUNNING WITH SCRIPT ----------------------
-for num, arg in enumerate(sys.argv):
-  print num, arg
- 
-dumpAll(fileAll=sys.argv[1],numb=sys.argv[2])
