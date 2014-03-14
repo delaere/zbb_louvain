9849c7910ce97fb88b49ed2229f2e66e3f43a69c
acaudron <adrien.caudron@cern.ch>
2014-03-14 11:17:13 +0100

    Bug fix for b-tag and lept. rew. for RDS production.

--- python/LeptonsReweightingControlPlots.py
+++ python/LeptonsReweightingControlPlots.py
@@ -7,9 +7,9 @@ class LeptonsReweightingControlPlots(BaseControlPlots):
     def __init__(self, dir=None, category=None, dataset=None, mode="plots"):
       # create output file if needed. If no file is given, it means it is delegated
       BaseControlPlots.__init__(self, dir=dir, purpose="LeptonsReweighting", dataset=dataset, mode=mode)
-      self.category = [int(s) for s in dir.GetName().split('_') if s.isdigit()]
-      if len(self.category): self.category=self.category[0] 
-      else: self.category=None
+      #self.category = [int(s) for s in dir.GetName().split('_') if s.isdigit()]
+      #if len(self.category): self.category=self.category[0] 
+      #else: self.category=None
     
     def beginJob(self, muChannel = None):
       self._muChannel = muChannel
@@ -20,11 +20,11 @@ class LeptonsReweightingControlPlots(BaseControlPlots):
       """LeptonsReweightingControlPlots"""
       result = { }
       if self._muChannel is None:
-        result["weight"] = event.weight(weightList=["Leptons"], category=self.category)
+        result["weight"] = event.weight(weightList=["Leptons"])
       elif self._muChannel == False:
-        result["weight"] = event.weight(weightList=["Leptons"], category=self.category, forceMode="Electron")
+        result["weight"] = event.weight(weightList=["Leptons"], forceMode="Electron")
       elif self._muChannel == True:
-        result["weight"] = event.weight(weightList=["Leptons"], category=self.category, forceMode="Muon")
+        result["weight"] = event.weight(weightList=["Leptons"], forceMode="Muon")
       return result
 
 if __name__=="__main__":
