b9f184556907ffce4fe7c41a7c3f3accacd8c953
Brieuc Francois <brieucfrancois@hotmail.com>
2014-05-15 18:00:28 +0200

    Implement more recent MET Phi Corrections

--- python/PATconfig/met_cff.py
+++ python/PATconfig/met_cff.py
@@ -1,6 +1,7 @@
 from PhysicsTools.PatAlgos.tools.metTools import *
 from PhysicsTools.PatUtils.tools.metUncertaintyTools import runMEtUncertainties
 
+
 def setupPatMets (process, runOnMC, makeNoPUMet):
 	process.preMetSequence = cms.Sequence()
 	
@@ -16,8 +17,40 @@ def setupPatMets (process, runOnMC, makeNoPUMet):
 	process.patPFJetMETtype1p2Corr.skipEM = cms.bool(False)
 	process.patPFJetMETtype1p2Corr.skipMuons = cms.bool(False)
 	#SysShift correction to turn the MET flat(ter) with respect to Phi
-	process.load("JetMETCorrections.Type1MET.pfMETsysShiftCorrections_cfi")
+	process.load("UserCode.zbb_louvain.PATconfig.METphiCorrections_cfi")
 	process.selectedVerticesForMEtCorr.src = cms.InputTag('goodPV')
+
+	if runOnMC :
+                process.PFMETSysShiftCorr = process.pfMEtSysShiftCorr.clone(parameter=process.NewPFMEtSysShiftCorrParameters_2012runABCDvsNvtx_mc,
+                                                                            )
+
+                process.MVAMETNURSysShiftCorr = process.pfMEtSysShiftCorr.clone(parameter=process.NewMVAMEtNURSysShiftCorrParameters_2012runABCDvsNvtx_mc,
+                                                                             src = cms.InputTag('pfMEtMVA')  # I think this is not mandatory for Nvtx based correction but to be safe...
+                                                                            )
+
+                process.MVAMETURSysShiftCorr = process.pfMEtSysShiftCorr.clone(parameter=process.NewMVAMEtURSysShiftCorrParameters_2012runABCDvsNvtx_mc,
+                                                                             src = cms.InputTag('pfMEtMVA') 
+                                                                            )
+
+                process.NoPUMETSysShiftCorr = process.pfMEtSysShiftCorr.clone(parameter=process.NewNoPUMEtSysShiftCorrParameters_2012runABCDvsNvtx_mc,
+                                                                             src = cms.InputTag('noPileUpPFMEt') 
+                                                                            )
+        else :
+
+                process.PFMETSysShiftCorr = process.pfMEtSysShiftCorr.clone(parameter=process.NewPFMEtSysShiftCorrParameters_2012runABCDvsNvtx_data,
+                                                                            )
+
+                process.MVAMETNURSysShiftCorr = process.pfMEtSysShiftCorr.clone(parameter=process.NewMVAMEtNURSysShiftCorrParameters_2012runABCDvsNvtx_data,
+                                                                             src = cms.InputTag('pfMEtMVA')
+                                                                            )
+
+                process.MVAMETURSysShiftCorr = process.pfMEtSysShiftCorr.clone(parameter=process.NewMVAMEtURSysShiftCorrParameters_2012runABCDvsNvtx_data,
+                                                                             src = cms.InputTag('pfMEtMVA')
+                                                                            )
+
+                process.NoPUMETSysShiftCorr = process.pfMEtSysShiftCorr.clone(parameter=process.NewNoPUMEtSysShiftCorrParameters_2012runABCDvsNvtx_data,
+                                                                             src = cms.InputTag('noPileUpPFMEt')
+                                                                            )
         
 	#MVA MET                        !!! Based on AK5PFJets and not on AK5PFchsJets (the same holds for noPUMET)
 	process.load("RecoMET.METPUSubtraction.mvaPFMET_cff")
@@ -36,7 +69,7 @@ def setupPatMets (process, runOnMC, makeNoPUMet):
 				    makeType1p2corrPFMEt=False,
 				    doApplyType0corr=True,
 				    jetCorrLabel = "L3Absolute",
-				    sysShiftCorrParameter=process.pfMEtSysShiftCorrParameters_2012runABCDvsNvtx_mc,
+				    sysShiftCorrParameter=process.pfMEtSysShiftCorrParameters_2012runABCDvsNvtx_mc,  #old phi correction for PFMET
 				    doApplySysShiftCorr=True,
 				    jetCorrPayloadName='AK5PFchs',
 				    makePFMEtByMVA=True,
@@ -67,7 +100,13 @@ def setupPatMets (process, runOnMC, makeNoPUMet):
 	process.pfCandsNotInJet.topCollection = cms.InputTag("pfNoTau")
 	
 	#Add Met with different corrections
-	process.patType01SCorrectedPFMet = process.patType1CorrectedPFMet.clone()
+	process.patType01SCorrectedPFMet = process.patType1CorrectedPFMet.clone(
+						srcType1Corrections = cms.VInputTag(
+                    						cms.InputTag('patPFJetMETtype1p2Corr', 'type1'),
+                    						cms.InputTag('patPFMETtype0Corr'),
+								cms.InputTag('PFMETSysShiftCorr')
+                						),
+ 	)
 
 	process.patType01CorrectedPFMet = cms.EDProducer("CorrectedPATMETProducer",
                 src = cms.InputTag('patPFMet'),
@@ -101,19 +140,17 @@ def setupPatMets (process, runOnMC, makeNoPUMet):
                 src = cms.InputTag('patPFMet'),
                 applyType1Corrections = cms.bool(True),
                 srcType1Corrections = cms.VInputTag(
-                    cms.InputTag('pfMEtSysShiftCorr')
+                    cms.InputTag('PFMETSysShiftCorr')
 		),
                 applyType2Corrections = cms.bool(False)
         )
-
-
- 	
+	
 	process.patType0sysCorrectedPFMet = cms.EDProducer("CorrectedPATMETProducer",
                 src = cms.InputTag('patPFMet'),
                 applyType1Corrections = cms.bool(True),
                 srcType1Corrections = cms.VInputTag(
                     cms.InputTag('patPFMETtype0Corr'),
-		    cms.InputTag('pfMEtSysShiftCorr')
+		    cms.InputTag('PFMETSysShiftCorr')
                 ),
                 applyType2Corrections = cms.bool(False)
         )       
@@ -123,13 +160,39 @@ def setupPatMets (process, runOnMC, makeNoPUMet):
                 applyType1Corrections = cms.bool(True),
                 srcType1Corrections = cms.VInputTag(
 		    cms.InputTag('patPFJetMETtype1p2Corr', 'type1'),
-                    cms.InputTag('pfMEtSysShiftCorr')
+                    cms.InputTag('PFMETSysShiftCorr')
                 ),
                 applyType2Corrections = cms.bool(False)
-        )  
+        ) 
 	
+	process.patMVAMETNURphiCorrected = cms.EDProducer("CorrectedPATMETProducer",
+                src = cms.InputTag('patPFMetMVA'),
+                applyType1Corrections = cms.bool(True),
+                srcType1Corrections = cms.VInputTag(
+                    cms.InputTag('MVAMETNURSysShiftCorr')
+                ),
+                applyType2Corrections = cms.bool(False)
+        )
 
-	process.metUncertaintySequence += cms.Sequence(process.patType01SCorrectedPFMet+process.patTypeOnly1CorrectedPFMet+process.patType0CorrectedPFMet+process.patTypeSysCorrectedPFMet+process.patType01CorrectedPFMet+process.patType1sysCorrectedPFMet+process.patType0sysCorrectedPFMet)
+        process.patMVAMETURphiCorrected = cms.EDProducer("CorrectedPATMETProducer",
+                src = cms.InputTag('patPFMetMVA'),
+                applyType1Corrections = cms.bool(True),
+                srcType1Corrections = cms.VInputTag(
+                    cms.InputTag('MVAMETURSysShiftCorr')
+                ),
+                applyType2Corrections = cms.bool(False)
+        )
+
+        process.patNoPUMETphiCorrected = cms.EDProducer("CorrectedPATMETProducer",
+                src = cms.InputTag('patPFMetNoPileUp'),
+                applyType1Corrections = cms.bool(True),
+                srcType1Corrections = cms.VInputTag(
+                    cms.InputTag('NoPUMETSysShiftCorr')
+                ),
+                applyType2Corrections = cms.bool(False)
+        )	
+
+	process.metUncertaintySequence += cms.Sequence(process.PFMETSysShiftCorr+process.MVAMETNURSysShiftCorr+process.MVAMETURSysShiftCorr+process.NoPUMETSysShiftCorr+process.patType01SCorrectedPFMet+process.patTypeOnly1CorrectedPFMet+process.patType0CorrectedPFMet+process.patTypeSysCorrectedPFMet+process.patType01CorrectedPFMet+process.patType1sysCorrectedPFMet+process.patType0sysCorrectedPFMet+process.patMVAMETNURphiCorrected+process.patMVAMETURphiCorrected+process.patMVAMETURphiCorrected+process.patNoPUMETphiCorrected)
  
 
 	#clean metUncertaintySequence
@@ -161,8 +224,3 @@ def setupPatMets (process, runOnMC, makeNoPUMet):
 
 
 
-
-
-
-
- 
