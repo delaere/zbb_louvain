a2fc608190ea703909bb534f8f3fa7bb1987f636
nadjieh <ajafari@cern.ch>
2014-07-01 15:08:29 +0200

    first version of haammbb code

---
+++ python/hambConfig.py
@@ -0,0 +1,15 @@
+
+from hambConfig_data import *
+
+#add to the data configuration the MC ones
+updateConfMC(c=configuration)
+
+# fine-tuning of the event content for display
+eventDumpConfig.collectionsToHide.append("genParticles")
+
+class configuration(configuration):
+    # my variables: files, systematics and other options
+    JERfactor = 1.
+    JESfactor = 0.
+    LeptonTnPfactor = 0 # Lepton reweighting uncertainty
+    SF_uncert="mean" #btagging reweighting:  choose among min/max/mean 
