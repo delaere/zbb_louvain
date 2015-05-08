c22fb031b9be261d754cf925b08a43112f4c92bb
Adrien Caudron <adrien.caudron@cern.ch>
2015-05-08 15:53:50 +0200

    Update llbbYieldFromTree.py

--- python/pyrootScripts/llbbYieldFromTree.py
+++ python/pyrootScripts/llbbYieldFromTree.py
@@ -1,14 +1,16 @@
+## This script compute the yields in each bin to be used for the limit computation and store them in a tree ##
+
+import os
+import array
+from math import sqrt
 from ROOT import *
 from llbbOptions import *
-import array
-import os
-from math import sqrt
 formulaName = "formulaPol3_C.so"
 gSystem.Load(formulaName)
 gROOT.SetBatch()
 
 class options_(options_):
-    #list of samples
+    ### list of samples ###
     samples = [
         "DATA",
         "DYjets",
@@ -25,7 +27,7 @@ class options_(options_):
         ]
     print samples
     
-    #Systematics
+    ### Systematics ###
     SYST = "Nominal"
     #SYST = "JESup"
     #SYST = "JESdown"
@@ -35,38 +37,44 @@ class options_(options_):
     #SYST = "BTAG_bc_down"
     #SYST = "BTAG_light_up"
     #SYST = "BTAG_light_down"
+
+    ### if True: will compute the stat. yields unc. instead of the yields ###
+    err = False
+
+    ### input files path ###
     path = options_.path.replace("SYST",SYST)
     
-    #output
+    ### output ###
     output = options_.output.replace("SYST",SYST).replace("V3","V4")
 
+### function which gets the input trees ###
 def createTree(files={"test" : "test.root"}, options=None):
     if options is None : return None
     Trees = {}
-    #run over all the samples
+    ### run over all the samples ###
     for key in files:
         print "Make RDS for", key
-        #Get file and tree
+        ### Get file and tree ###
         tf = TFile.Open(files[key])
 	Trees[key] = tf.Get("rds_zbb")
     return Trees
 
-def makeRF(output, Trees, options): #opt_cut, categories, dirLmits):
+### function to fill the output tree ###
+def makeRF(output, Trees, options):
+    ### define final tree ###
     tree = TTree("yields","tree containing all informations for yields")
 
+    ### create the branches for the masses ###
     print "create RootFile "
     mA = array.array('d', [0.0])
     mH = array.array('d', [0.0])
     tree.Branch('mA',mA,'mA/D')
     tree.Branch('mH',mH,'mH/D')
-    #Prepare the output roofiles
-    #output.mkdir(cat+rep,cut)
-    #output.cd(cat+rep)
-    #Get the keys sorted
-    keys = [key for key in Trees ] #if cat in key]
+    ### Get the keys sorted ###
+    keys = [key for key in Trees]
     keys.sort()
-    #Initiate the string to be included in the txt files to compute the limits
     a = {}
+    ### creating branches for the yields ###
     print "creating branches "
     for key in keys:
 	for cat in options.categories :
@@ -82,12 +90,12 @@ def makeRF(output, Trees, options): #opt_cut, categories, dirLmits):
 	    else :
                 a[key+cat] = array.array('d', [0.0])
 	        tree.Branch(key+cat,a[key+cat],key+cat+'/D')
-	    #print 'key : ', key+cat
 
     print 'run over the samples'
-    #Run over the samples
+    ### Run over the samples ###
     for cutkey in options.cut :
         print 'cutkey : ', cutkey
+        ### get M_A and M_H ###
         mH[0] = float(options.mH_list[cutkey])
         mA[0] = float(options.mA_list[cutkey])
         #if int(mH[0]) != 216 : continue
@@ -98,24 +106,27 @@ def makeRF(output, Trees, options): #opt_cut, categories, dirLmits):
                 print "key is ", key
                 for cat in options.categories:
                     print "cat is ", cat
-                    #Select phase space
-                    #tmpTH1 = TH1D('MET','MET',100,0,1000)
+                    ### define reweighting ###
                     if "DATA" in key : reweighting = "" 
 		    else :
 			condition = options.SYST
 			reweighting = options.rewForm["Zbb"][cat]
 		        if "TT" in key : reweighting += SFlist[condition]["TT"]
-                    err = True
+                    err = options.err
+                    ### compute the yields ###
 		    if "DY" in key:
 			condition = options.SYST
+                        ### get reweighting ###
 			reweighting_Zbb = reweighting+SFlist[condition]["Zbb"]+"*formula(boostselectionZbbM,jetmetbjet1Flavor,jetmetbjet2Flavor)"
 			reweighting_Zbx = reweighting+SFlist[condition]["Zbx"]+"*formula(boostselectionZbbM,jetmetbjet1Flavor,jetmetbjet2Flavor)"
 			reweighting_Zxx = reweighting+SFlist[condition]["Zxx"]+"*formula(boostselectionZbbM,jetmetbjet1Flavor,jetmetbjet2Flavor)"
 
+                        ### create histo + sumw2 + fill histo with proper cut and reweighting and unc. computation ###
 			tmpTH1 = TH1D('MET','MET',100,0,1000)
                         tmpTH1.Sumw2();
 			numEnt = Trees[key].Draw("jetmetMET >> MET","(abs(jetmetbjet1Flavor)==5 && abs(jetmetbjet2Flavor)==5 && jetmetnj==2 && "+options.cut[cutkey]+" && "+options.categories[cat]+")"+reweighting_Zbb)
                         tmpArray = tmpTH1.GetSumw2()
+                        ### get yields or unc. normalised to lumi and x-section ###
                         sum = 0
                         for arr in range(0,tmpArray.GetSize()) : sum+=tmpArray[arr]
                         if not err : a["Zbb2j"+cat][0] = tmpTH1.Integral()*(lumi["DATA"]/lumi["Zbb"])
@@ -159,24 +170,15 @@ def makeRF(output, Trees, options): #opt_cut, categories, dirLmits):
 			tmpTH1 = TH1D('MET','MET',100,0,1000)
                         tmpTH1.Sumw2();
                         numEnt = Trees[key].Draw("jetmetMET >> MET","("+options.cut[cutkey]+" && "+options.categories[cat]+")"+reweighting)
-                        #print "("+options.cut[cutkey]+" && "+options.categories[cat]+")"+reweighting
                         tmpArray = tmpTH1.GetSumw2()
                         sum = 0
                         for arr in range(0,tmpArray.GetSize()) : sum+=tmpArray[arr]
                         if not err : a[key+cat][0] = tmpTH1.Integral()*(lumi["DATA"]/lumi[key])
                         else : a[key+cat][0] = sqrt(sum)*(lumi["DATA"]/lumi[key])
                         #print key+cat, numEnt, a[key+cat][0], (lumi["DATA"]/lumi[key])
-                        #print "Option Mll", mA[0], mH[0], a[key+cat][0]
                         tmpTH1.Delete()
-                        #tmpTH1 = TH1D('MET','MET',100,0,1000)
-                        #Trees[key].Draw("jetmetMET >> MET","("+options.cut[cutkey]+" && "+options.categories[cat]+"&&boostselectionbestzmassMu>76&&boostselectionbestzmassMu<106)"+reweighting)
-                        #print "("+options.cut[cutkey]+" && "+options.categories[cat]+"&&boostselectionbestzmassMu>76&&boostselectionbestzmassMu<106)"+reweighting
-                        #print "Small Mll", mA[0], mH[0], tmpTH1.Integral()*(lumi["DATA"]/lumi[key])
-                        #tmpTH1.Delete()
-	                #print 'a[key+cat] :', a[key+cat]
             tree.Fill()
-            #if a["TTFullLeptMu"][0]>1 : break         
-    #Write output
+    ### Write output ###
     tree.Write()
     output.Write()
     output.Close()
@@ -186,20 +188,19 @@ def main():
     print "Start main"
     options = options_()
     print options.samples
-    #get file name
+    ### get file name ###
     files = {}
     for sample in options.samples:
 	if "DY" in sample : files[sample] = options.path.replace("NAME","DY")
         elif "DATA" not in sample : files[sample] = options.path.replace("NAME",sample)
         else : files[sample] = options.path_data
         print files[sample]
-    #get RDS
+    ### get trees ### 
     Trees = createTree(files=files, options=options)
-    #RDS = bkgNorm(RDS)
-    #Make and store hists
+    ### Create output ###
     output = TFile(options.output,"RECREATE")
     print Trees
-    #for key in options.cut : makeRF(output, RDS, key, options.cut[key], options.categories, options.dirLimits)
+    ### calculate yields ###
     makeRF(output, Trees, options)
     return
 
