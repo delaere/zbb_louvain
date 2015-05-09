074bdda60a0b923f45a58604d7c17b52c629f65e
Adrien Caudron <adrien.caudron@cern.ch>
2015-05-09 13:11:20 +0200

    llbbROC.py

--- python/pyrootScripts/llbbROC.py
+++ python/pyrootScripts/llbbROC.py
@@ -1,9 +1,13 @@
+## Script to compute the ROC curve for a given 1D cut ##
+
 from ROOT import *
 
 def main():
+    ### Get CPs for the MC and signal where the verible to test is plotted ###
     f_bkgs = TFile("../../test/Zbb_Rew_ZbbnoMETcut_smallMll_RewFormformulaPol3_NotNorm_testRew.root")
     f_sig = TFile("/nfs/user/acaudron/ControlPlots/cp5314p1/AllRDS/Nominal/RDS_ZA_575_142/ZA_575_142_Summer12_final_skimed_zmet.root")
 
+    ### Get the variable to test for the bkgs ###
     metsig = f_bkgs.Get("Combined/jetmetMETsignificance")
     prim = metsig.GetListOfPrimitives()
     for obj in prim:
@@ -12,10 +16,12 @@ def main():
             break
     bkgs = stack.GetStack().Last()
 
+    ### Get the variable to test for the signal ###
     t_sig = f_sig.Get("rds_zbb")
     sig = TH1D("sig","sig",80,0,40)
     t_sig.Draw("jetmetMETsignificance>>sig","(((rc_stage_8_idx&&boostselectionbestzmassMu>76&&boostselectionbestzmassMu<106))||((rc_stage_19_idx&&boostselectionbestzmassEle>76&&boostselectionbestzmassEle<106)))*leptonsReweightingweight*lumiReweightingLumiWeight*mcReweightingweight*btaggingReweightingMM")
 
+    ### define the TGraphs ###
     effeff = TGraph(len(range(0,bkgs.GetNbinsX()+2)))
     effeff.SetName("effeff")
 
@@ -29,15 +35,18 @@ def main():
     roc.SetName("roc")
 
     print bkgs.Integral(0,bkgs.GetNbinsX()+1), sig.Integral(0,sig.GetNbinsX()+1)
+    ### make the 1D scan ###
     for bin in reversed(range(0,bkgs.GetNbinsX()+2)):
-        #print bin
         metcut = bkgs.GetXaxis().GetBinUpEdge(bin)
+        ### compute the eff. ###
         eff_bkgs = bkgs.Integral(0,bin)/bkgs.Integral(0,bkgs.GetNbinsX()+1)
         eff_sig = sig.Integral(0,bin)/sig.Integral(0,sig.GetNbinsX()+1)
+        ### compute the figure of merit ###
         fom = sqrt(bkgs.Integral(0,bin)+10*sig.Integral(0,bin))-sqrt(bkgs.Integral(0,bin))
         #if bkgs.Integral(0,bin)>0 : fom = sqrt(bkgs.Integral(0,bin)+sig.Integral(0,bin))/sqrt(bkgs.Integral(0,bin))
         #else : fom = 0.
         print eff_sig, eff_bkgs
+        ### fill the graphs ###
         effeff.SetPoint(bin,eff_sig,eff_bkgs)
         sigEff.SetPoint(bin,metcut,eff_sig)
         bkgEff.SetPoint(bin,metcut,eff_bkgs)
@@ -45,6 +54,7 @@ def main():
         print bin, metcut, fom
 
     k = TCanvas()
+    ### plot graphs and get the point of max sign. ###
     #roc.Draw("AL")
     print "max is:", TMath.MaxElement(roc.GetN(),roc.GetY())
     sigEff.Draw("AL")
